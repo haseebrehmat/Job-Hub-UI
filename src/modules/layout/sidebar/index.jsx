37b1096a5a737e073df7153ad6e1f961b81f77a9
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-02-09 21:11:32 +0500

    Convert anchor to Link

--- src/modules/layout/sidebar/index.jsx
+++ src/modules/layout/sidebar/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { menuItems } from '@constants/layout'
 
 import { NavigationIcon } from '@icons'
@@ -9,7 +10,7 @@ const SideBar = ({ toggle, setToggle }) => (
             toggle ? 'w-auto lg:w-[15%]' : 'w-fit'
         } flex-col space-y-2 border-2 py-2 sm:p-2 my-2 ml-2 text-[#048C8C] bg-[#EDFFFB] rounded-lg`}
     >
-        <div className={`p-2 flex justify-center md:justify-${toggle ? 'between' : 'center'}`}>
+        <div className={`p-2 flex justify-center md:justify-${toggle ? 'center' : 'between'}`}>
             {toggle ? (
                 <span className='hidden lg:block'>
                     <OctagonLogo />
@@ -24,14 +25,14 @@ const SideBar = ({ toggle, setToggle }) => (
         <hr className='w-50 h-0.5 bg-[#048C8C] my-4 border-0 rounded' />
         {menuItems?.length > 0 &&
             menuItems?.map(item => (
-                <a
+                <Link
                     key={item.label}
-                    href={item.link}
+                    to={item.link}
                     className='flex items-center text-sm p-4 text-[#003C40] rounded dark:text-white border-2 hover:text-[#003C40] border-solid border-[#EDFFFB] hover:border-2 hover:border-solid hover:border-[#048C8C] active:border-2 active:border-solid active:border-[#048C8C]'
                 >
                     {item.svg}
                     {toggle ? <span className='ml-3 hidden lg:block'>{item.label}</span> : ''}
-                </a>
+                </Link>
             ))}
         <hr className='w-50 h-0.5 bg-[#048C8C] my-4 border-0 rounded' />
         <div className='flex flex-row h-[100%]'>
