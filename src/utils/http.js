6c895a6fb0b76e05adf5e8cf5ad20641ab3f4b73
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-02-10 17:21:28 +0500

    Embedd .env (s) wip

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
 const http = axios.create({
-    baseURL: 'http://18.144.164.235:8000/',
+    baseURL: import.meta.env.VITE_AUTH_API_URL,
     headers: {
         Accept: 'application/json',
     },
 })
 
 const scrapperHttp = axios.create({
-    baseURL: 'http://54.183.171.9/',
+    baseURL: import.meta.env.VITE_SCRAPPER_API_URL,
     headers: {
         Accept: 'application/json',
     },
