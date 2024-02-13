dc0b2fa92182ae3ae988a4c51f0eec9f7a19bb98
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-02-13 21:04:26 +0500

    OC-14: Validate login on frontend

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,17 +1,20 @@
 import { memo } from 'react'
 
-const Input = memo(({ name, type = 'text', onChange = null, value = undefined, ph = '', classes = null }) => (
-    <div>
-        <input
-            type={type}
-            name={name}
-            className={`bg-gray-50 border border-cyan-600 text-gray-900 sm:text-sm rounded-lg focus:ring-[#048C8C] focus:border-[#048C8C] block w-full p-2.5 ${classes}`}
-            placeholder={ph}
-            required=''
-            value={value}
-            onChange={onChange}
-        />
-    </div>
-))
+const Input = memo(
+    ({ name, type = 'text', onChange = null, onBlur = null, value = undefined, ph = '', classes = null }) => (
+        <div>
+            <input
+                type={type}
+                name={name}
+                className={`bg-gray-50 border border-cyan-600 text-gray-900 sm:text-sm rounded-lg focus:ring-[#048C8C] focus:border-[#048C8C] block w-full p-2.5 ${classes}`}
+                placeholder={ph}
+                required=''
+                value={value}
+                onChange={onChange}
+                onBlur={onBlur}
+            />
+        </div>
+    )
+)
 
 export default Input
