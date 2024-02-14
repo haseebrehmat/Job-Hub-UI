454ce195abffd632e47e2f2c8d4770d3d1df5a1e
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-02-14 17:19:13 +0500

    Title on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { Link } from 'react-router-dom'
 import { Dashboard } from '@modules'
 
 const App = () => (
-    <div className='flex flex-col px-2'>
-        <Link to='applied-jobs' className='text-[#006366] font-medium text-2xl mb-1 -mt-5'>
-            Welcome to Octagon
-        </Link>
+    <div className='block px-2'>
         <Dashboard />
     </div>
 )
