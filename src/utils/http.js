cab291f6ac478c2c9614719f743db7d4aa75d09b
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-02-16 03:12:10 +0500

    fix: Change http for applied jobs

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,9 +1,13 @@
 import axios from 'axios'
+import { getToken } from './helpers'
+
+const token = getToken() || null
 
 const http = axios.create({
     baseURL: import.meta.env.VITE_AUTH_API_URL,
     headers: {
         Accept: 'application/json',
+        Authorization: `Bearer ${token}`,
     },
 })
 
