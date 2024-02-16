26a736c29f1d3df7a88370e4f158c4e4fd5cd958
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-02-16 20:00:31 +0500

    fix: Show empty msg when status is error

--- src/modules/dashboard/index.jsx
+++ src/modules/dashboard/index.jsx
@@ -18,16 +18,20 @@ const Dashboard = () => {
     ) : (
         <div className='flex flex-col w-full space-y-14'>
             <Filters filters={filters} setFilters={setFilters} />
-            <div className='flex items-start justify-between'>
-                <div className='flex flex-col w-4/5 space-y-16'>
-                    <Leads data={data.leads} />
-                    <TechStacks data={data.tech_jobs} />
-                    <WarmLeads data={data.leads} />
+            {data.status === 'success' ? (
+                <div className='flex items-start justify-between'>
+                    <div className='flex flex-col w-4/5 space-y-16'>
+                        <Leads data={data?.leads} />
+                        <TechStacks data={data?.tech_jobs} />
+                        <WarmLeads data={data?.leads} />
+                    </div>
+                    <div className='w-1/5 pl-6'>
+                        <Statistics data={data?.statistics} />
+                    </div>
                 </div>
-                <div className='w-1/5 pl-6'>
-                    <Statistics data={data.statistics} />
-                </div>
-            </div>
+            ) : (
+                <p className='mx-auto'>No Graphs or data Found!</p>
+            )}
         </div>
     )
 }
