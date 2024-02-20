4b4044e033e4b8b28a2a0c1f6ef479beee2701f9
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-02-20 15:10:55 +0500

    Fix: wrong password validation & axios inceptors

--- src/modules/authentication/api/index.js
+++ src/modules/authentication/api/index.js
@@ -2,14 +2,11 @@ import { http } from '@utils/http'
 import { getMsg, saveToken, saveRefreshToken } from '@utils/helpers'
 
 export const loginUser = (email, password) =>
-    http
-        .post('api/auth/login/', { email, password })
-        .then(res => {
-            saveToken(res.data.access)
-            saveRefreshToken(res.data.refresh)
-            return { status: 'success', message: 'User logged in successfully!' }
-        })
-        .catch(error => ({ status: 'error', message: getMsg(error) }))
+    http.post('api/auth/login/', { email, password }).then(res => {
+        saveToken(res.data.access)
+        saveRefreshToken(res.data.refresh)
+        return { status: 'success', message: 'User logged in successfully!' }
+    })
 
 export const sendResetPasswordLink = email =>
     http
