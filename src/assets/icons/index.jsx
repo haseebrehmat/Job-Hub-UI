16c32735b7b8301741e28a0566dae7afd2042f9a
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-02-24 17:19:39 +0500

    feat: Add roles option in sidenave & role listing wip

--- src/assets/icons/index.jsx
+++ src/assets/icons/index.jsx
@@ -35,6 +35,7 @@ import { CgSidebarOpen } from 'react-icons/cg'
 import { IoNewspaperSharp } from 'react-icons/io5'
 import { FaBriefcase, FaHotjar, FaUsers } from 'react-icons/fa'
 import { BiPlusCircle } from 'react-icons/bi'
+import { RiUserSettingsLine } from 'react-icons/ri'
 
 export const LogoutIcon = <MdOutlineLogout style={{ fontSize: '22px' }} />
 export const CloseIcon = <MdClose style={{ fontSize: '22px' }} />
@@ -75,5 +76,6 @@ export const TeamAppliedJobsIcon = <BsCollectionFill style={{ fontSize: '22px' }
 export const OpenSubMenuIcon = <MdOutlineKeyboardArrowDown style={{ fontSize: '22px' }} />
 export const UserIcon = <FiUser style={{ fontSize: '22px' }} />
 export const CompaniesIcon = <FaUsers style={{ fontSize: '22px' }} />
+export const RolesIcon = <RiUserSettingsLine style={{ fontSize: '22px' }} />
 export const CreateIcon = <BiPlusCircle style={{ fontSize: '22px' }} />
 export const ActionsIcons = <BsThreeDotsVertical style={{ fontSize: '20px' }} />
---
+++ src/modules/userManagement/roles/index.jsx
@@ -0,0 +1,67 @@
+import { memo, useState } from 'react'
+import useSWR from 'swr'
+
+import { Loading, Searchbox, EmptyTable, Filters, Button } from '@components'
+
+import { CompanyForm } from '@modules/userManagement/components'
+import { fetchCompanies } from '@modules/userManagement/api'
+
+import { roleHeads } from '@constants/userManagement'
+
+import { CreateIcon, ActionsIcons } from '@icons'
+
+const Roles = () => {
+    const [query, setQuery] = useState()
+    const [company, setCompany] = useState()
+    const [show, setShow] = useState(false)
+    const { data, error, isLoading, mutate } = useSWR('/api/auth/company/', fetchCompanies)
+    const handleClick = ({ name, status, id }) => {
+        setCompany({ name, status, id })
+        setShow(!show)
+    }
+    if (isLoading) return <Loading />
+    return (
+        <div className='max-w-full overflow-x-auto mb-14 px-5'>
+            <div className='flex items-center space-x-4 py-6'>
+                <Searchbox query={query} setQuery={setQuery} />
+                <Filters />
+                <Button
+                    label='Create Role'
+                    fit
+                    icon={CreateIcon}
+                    onClick={() => handleClick({ name: '', status: true, user: '' })}
+                />
+            </div>
+            <table className='table-auto w-full text-sm text-left text-[#048C8C]'>
+                <thead className='text-xs uppercase border border-[#048C8C]'>
+                    <tr>
+                        {roleHeads.map(heading => (
+                            <th scope='col' className='px-3 py-4' key={heading}>
+                                {heading}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {data?.companies?.length > 0 && !error ? (
+                        data.companies.map((comp, idx) => (
+                            <tr className='bg-white border-b border-[#006366] border-opacity-30' key={comp.id}>
+                                <td className='px-3 py-6'>{idx + 1}</td>
+                                <td className='px-3 py-6'>{comp?.name}</td>
+                                <td className='px-3 py-6'>{comp?.code}</td>
+                                <td className='px-3 py-6 float-right' onClick={() => handleClick(comp)}>
+                                    {ActionsIcons}
+                                </td>
+                            </tr>
+                        ))
+                    ) : (
+                        <EmptyTable cols={6} msg='No roles found yet!' />
+                    )}
+                </tbody>
+            </table>
+            {show && <CompanyForm show={show} setShow={setShow} mutate={mutate} company={company} />}
+        </div>
+    )
+}
+
+export default memo(Roles)
