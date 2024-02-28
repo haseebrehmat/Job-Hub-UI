1bff4d0bd1d98f104ef306bcf03a65854c750b61
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-02-28 17:04:09 +0500

    feat: Users crud without API

--- src/modules/userManagement/components/index.jsx
+++ src/modules/userManagement/components/index.jsx
@@ -1,4 +1,5 @@
 import CompanyForm from '@modules/userManagement/components/CompanyForm'
 import RoleForm from '@modules/userManagement/components/RoleForm'
+import UserForm from '@modules/userManagement/components/UserForm'
 
-export { CompanyForm, RoleForm }
+export { CompanyForm, RoleForm, UserForm }
