98622b1b42a11e3196d0a168169ac073a2b31db7
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-02-28 20:06:12 +0500

    feat: table heads changed

--- src/utils/constants/userManagement.js
+++ src/utils/constants/userManagement.js
@@ -1,6 +1,6 @@
 export const comapnyHeads = ['ID', 'Name', 'Company COde', 'Status', '']
 export const roleHeads = ['ID', 'Name', 'Description', 'Code', '']
-export const userHeads = ['ID', 'Name', 'Email', 'Username', 'Role', 'Company', '']
+export const userHeads = ['ID', 'Email', 'Username', 'Role', 'Company', '']
 
 export const comapnyStatus = {
     0: 'Enable',
