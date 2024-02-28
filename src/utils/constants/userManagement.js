72eb4f1e7d4d8575ca1dde2f09f84b1ec6a03ced
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-02-28 21:18:40 +0500

    feat: groups in role wip

--- src/utils/constants/userManagement.js
+++ src/utils/constants/userManagement.js
@@ -1,5 +1,5 @@
 export const comapnyHeads = ['ID', 'Name', 'Company COde', 'Status', '']
-export const roleHeads = ['ID', 'Name', 'Description', 'Code', '']
+export const roleHeads = ['ID', 'Name', 'Description', 'Group', 'Code', '']
 export const userHeads = ['ID', 'Email', 'Username', 'Role', 'Company', '']
 
 export const comapnyStatus = {
