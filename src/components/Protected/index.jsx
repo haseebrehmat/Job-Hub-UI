25d5163907f6225b78303c737aa8219039e3e3c5
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-03-04 00:15:51 +0500

    feat: embedd route level permissions

--- src/components/Protected/index.jsx
+++ src/components/Protected/index.jsx
@@ -1,12 +1,21 @@
 import { Navigate, useLocation } from 'react-router-dom'
 
-import { getToken } from '@utils/helpers'
+import { getToken, can } from '@utils/helpers'
 
-const Protected = ({ children }) => {
+const Protected = ({ children, permission }) => {
     const token = getToken()
     const location = useLocation()
+    const isAuthorized = permission === 'all' ? true : can(permission)
 
-    return token ? children : <Navigate to='/login' replace state={{ path: location.pathname }} />
+    return token ? (
+        isAuthorized ? (
+            children
+        ) : (
+            <Navigate to='/' />
+        )
+    ) : (
+        <Navigate to='/login' replace state={{ path: location.pathname }} />
+    )
 }
 
 export default Protected
