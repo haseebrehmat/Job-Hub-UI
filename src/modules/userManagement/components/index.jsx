9bf6a8c133acd2ff010174376bda3695b6f54ab2
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-03-03 15:21:51 +0500

    feat: add permissions component to user management list

--- src/modules/userManagement/components/index.jsx
+++ src/modules/userManagement/components/index.jsx
@@ -1,5 +1,6 @@
 import CompanyForm from '@modules/userManagement/components/CompanyForm'
 import RoleForm from '@modules/userManagement/components/RoleForm'
 import UserForm from '@modules/userManagement/components/UserForm'
+import Permissions from '@modules/userManagement/components/Permissions'
 
-export { CompanyForm, RoleForm, UserForm }
+export { CompanyForm, RoleForm, UserForm, Permissions }
