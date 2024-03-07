3258bf83fbd65c41dc17b63d689e20fa0f38d79d
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-03-07 19:35:24 +0500

    job portal FE done without code refactoring

--- src/modules/jobsFilter/components/index.jsx
+++ src/modules/jobsFilter/components/index.jsx
@@ -1,4 +1,3 @@
 import TableNavigate from '@modules/jobsFilter/components/TableNavigate'
-import FilterForm from '@modules/jobsFilter/components/FilterForm'
 
-export { TableNavigate, FilterForm }
+export { TableNavigate }
