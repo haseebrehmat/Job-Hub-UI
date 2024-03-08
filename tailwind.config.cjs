57b61ffca8a8578bdd2e274adecfc6de7a2c080b
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-03-08 20:27:09 +0500

    fix: remove flowbite and flowbite react

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /* eslint-disable global-require */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {
             fontFamily: {
@@ -9,5 +9,5 @@ module.exports = {
             },
         },
     },
-    plugins: [require('flowbite/plugin')],
+    plugins: [],
 }
