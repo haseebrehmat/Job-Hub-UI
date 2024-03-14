df70aa15c3d4585c155583c76a0aeda9765b05d0
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-03-14 20:29:31 +0500

    update: avatar and details hook

--- src/modules/profile/components/UpdateAvatar/index.jsx
+++ src/modules/profile/components/UpdateAvatar/index.jsx
@@ -1,53 +1,73 @@
-import { memo } from 'react'
-import useSWR from 'swr'
+import { memo, useState } from 'react'
 import { toast } from 'react-hot-toast'
-
-import { Loading, Button } from '@components'
-
-import { fetchProfile, updateProfile } from '@modules/profile/api'
 
 import { useMutate } from '@/hooks'
 
-import { profileSchema } from '@utils/schemas'
-import { getMsg } from '@utils/helpers'
+import { Button } from '@components'
 
-const UpdateAvatar = () => {
-    const { data, isLoading } = useSWR('/api/auth/user_profile/', fetchProfile)
-    const { values, errors, handleSubmit, handleChange, trigger } = useMutate(
-        '/api/auth/user_profile/',
-        updateProfile,
-        { file: data?.profile?.file || null },
-        profileSchema,
+import { FilePreview } from '@modules/profile/components'
+import { updateAvatar } from '@modules/profile/api'
+
+import { avatarSchema } from '@utils/schemas'
+import { getMsg } from '@utils/helpers'
+import { avatarPlaceholder } from '@constants/profile'
+
+const UpdateAvatar = ({ data }) => {
+    const [avatar] = useState(data?.profile?.file_url || avatarPlaceholder)
+    const [uploaded, setUploaded] = useState(null)
+
+    const { errors, handleSubmit, setFieldValue, trigger, wait } = useMutate(
+        '/api/auth/user_profile_image/',
+        updateAvatar,
+        { file: data?.profile?.file_url || avatarPlaceholder },
+        avatarSchema,
         async formValues => trigger({ ...formValues }),
-        error => toast.error(getMsg(error))
+        error => toast.error(getMsg(error)),
+        () => window.location.reload()
     )
 
-    if (isLoading) return <Loading />
-
     return (
-        <form onSubmit={handleSubmit} className='text-[#328d8c] flex items-center space-x-6 mx-auto mt-4'>
+        <form onSubmit={handleSubmit} className='text-[#328d8c]' encType='multipart/form-data'>
             <h1 className='mb-2 text-lg font-medium'>Avatar upload</h1>
             <hr className='mb-5' />
             <div className='grid grid-flow-row gap-2'>
-                <div className='shrink-0'>
-                    <img
-                        className='h-16 w-16 object-cover rounded-full'
-                        alt='Avatar'
-                        src={values.file ?? 'https://ui-avatars.com/api/?name=Avatar&background=0D8ABC&color=fff'}
-                    />
+                <div className='flex items-center space-x-6 mx-auto mt-4'>
+                    <a className='shrink-0' href={avatar} target='_blank' rel='noreferrer'>
+                        <img
+                            className='h-28 w-28 object-cover shadow-lg rounded-full border border-[#328d8c]'
+                            alt='Avatar'
+                            src={avatar}
+                        />
+                    </a>
+                    <label className='block'>
+                        <span className='sr-only'>Choose profile photo</span>
+                        <input
+                            type='file'
+                            name='file'
+                            accept='image/png, image/jpg, image/jpeg, image/webp'
+                            onChange={e => {
+                                setFieldValue('file', e.currentTarget.files[0])
+                                setUploaded(e.currentTarget.files[0])
+                            }}
+                            className='block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:text-white file:font-semibold file:bg-[#329988] hover:file:bg-[#4ab9a7]'
+                        />
+                    </label>
                 </div>
-                <label className='block'>
-                    <span className='sr-only'>Choose profile photo</span>
-                    <input
-                        type='file'
-                        onChange={handleChange}
-                        className='block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 hover:file:bg-violet-100'
-                    />
-                </label>
-                {errors.file && <small className='ml-1 text-xs text-red-600'>{errors.file}</small>}
-                <div className='pt-4 text-end'>
-                    <Button label='Update' type='submit' fit classes='px-6' />
-                </div>
+                {uploaded && (
+                    <div className='flex flex-col items-center mx-auto mt-4'>
+                        <FilePreview file={uploaded} />
+                        {errors.file && <small className='my-2 text-xs text-red-600'>{errors.file}</small>}
+                        <div className='pt-4'>
+                            <Button
+                                label={wait ? 'Uploading...' : 'Upload'}
+                                type='submit'
+                                fit
+                                classes='px-6'
+                                disabled={wait}
+                            />
+                        </div>
+                    </div>
+                )}
             </div>
         </form>
     )
