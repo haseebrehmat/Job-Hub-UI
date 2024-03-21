a6d22b06969ad4666451240a0ae55771a51bc59e
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-03-21 17:07:41 +0500

    feature: job posting in progress

---
+++ src/modules/jobsUploader/components/index.jsx
@@ -0,0 +1,4 @@
+import FilesUploader from '@modules/jobsUploader/components/filesUploader'
+import JobsPoster from '@modules/jobsUploader/components/jobForm'
+import jobForm from '@modules/coverLetter/components/Form'
+export { FilesUploader, JobsPoster, jobForm }
---
+++ src/modules/jobsUploader/components/jobPoster/index.jsx
@@ -0,0 +1,60 @@
+import { React, useState } from 'react'
+
+import { Loading, Badge, Searchbox, EmptyTable, Button } from '@components'
+import { CreateIcon, ActionsIcons } from '@icons'
+import { comapnyHeads, comapnyStatus } from '@constants/userManagement'
+
+const JobsPoster = () => {
+    const [query, setQuery] = useState('')
+    return (
+        <div className='max-w-full overflow-x-auto mb-14 px-5'>
+            <div className='flex items-center space-x-4 py-6'>
+                <Searchbox query={query} setQuery={setQuery} />
+                <Button
+                    label='Create Company'
+                    fit
+                    icon={CreateIcon}
+                    onClick={() => handleClick({ name: '', status: true })}
+                />
+
+            </div>
+            <table className='table-auto w-full text-sm text-left text-[#048C8C]'>
+                <thead className='text-xs uppercase border border-[#048C8C]'>
+                    <tr>
+                        {comapnyHeads.map(heading => (
+                            <th scope='col' className='px-3 py-4' key={heading}>
+                                {heading}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                {/* <tbody>
+                    {data?.companies?.length > 0 && !error ? (
+                        data.companies.map((comp, idx) => (
+                            <tr className='bg-white border-b border-[#006366] border-opacity-30' key={comp.id}>
+                                <td className='px-3 py-6'>{idx + 1}</td>
+                                <td className='px-3 py-6'>{comp?.name}</td>
+                                <td className='px-3 py-6'>{comp?.code}</td>
+                                <td className='px-1 py-6'>
+                                    <Badge
+                                        label={comapnyStatus[comp?.status ? 0 : 1]}
+                                        type={comp?.status ? 'enabled' : 'disabled'}
+                                    />
+                                </td>
+                                <td className='px-3 py-6 float-right' onClick={() => handleClick(comp)}>
+                                    {ActionsIcons}
+                                </td>
+                            </tr>
+                        ))
+                    ) : (
+                        <EmptyTable cols={6} msg='No companies found yet!' />
+                    )}
+                </tbody> */}
+            </table>
+                {/* <CompanyForm show={show} setShow={setShow} mutate={mutate} company={company} /> */}
+            </div>
+
+    )
+}
+
+export default JobsPoster
