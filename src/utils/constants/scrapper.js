018f073ffb103e37e1d020b99ca701c0bb0b60ff
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-03-22 16:05:05 +0500

    feat: add scrapper constants

--- src/utils/constants/scrapper.js
+++ src/utils/constants/scrapper.js
@@ -1,8 +1,31 @@
-export const options = [
-    { label: 'Seconds', value: 'seconds' },
+export const INTERVAL_TYPE_OPTIONS = [
     { label: 'Minutes', value: 'minutes' },
     { label: 'Hours', value: 'hours' },
     { label: 'Days', value: 'days' },
 ]
 
-export const settingsHeads = ['ID', 'Source', 'Type', 'Setting', '']
+export const JOB_SOURCE_OPTIONS = [
+    { label: 'Adzuna', value: 'adzuna' },
+    { label: 'Dice', value: 'dice' },
+    { label: 'Indeed', value: 'indeed' },
+    { label: 'Glassdoor', value: 'glassdoor' },
+    { label: 'LinkedIn', value: 'linkedin' },
+    { label: 'Simply Hired', value: 'simplyhired' },
+    { label: 'Monster', value: 'monster' },
+    { label: 'Zip Recruiter', value: 'ziprecruiter' },
+    { label: 'Career Builder', value: 'careerbuilder' },
+]
+
+export const JOB_SOURCES = {
+    linkedin: 'Linkedin',
+    careerbuilder: 'Career Builder',
+    adzuna: 'Adzuna',
+    dice: 'Dice',
+    indeed: 'Indeed',
+    ziprecruiter: 'Zip Recruiter',
+    glassdoor: 'Glassdoor',
+    monster: 'Monster',
+    simplyhired: 'Simply Hired',
+}
+
+export const SETTING_HEADS = ['ID', 'Source', 'Type', 'Setting', '']
