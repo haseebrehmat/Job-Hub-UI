f27b63a914fa8bb89c9371a665599786e3bb0887
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-03-28 17:11:06 +0500

    feat: replace cronjob component position

--- src/modules/scrapper/components/index.jsx
+++ src/modules/scrapper/components/index.jsx
@@ -1,6 +1,7 @@
+import CronjobSetting from '@modules/scrapper/components/CronjobSetting'
 import CronjobSettingActions from '@modules/scrapper/components/CronjobSettingActions'
 import CronjobSettingForm from '@modules/scrapper/components/CronjobSettingForm'
 import CronjobTypes from '@modules/scrapper/components/CronjobTypes'
 import SyncNow from '@modules/scrapper/components/SyncNow'
 
-export { CronjobSettingActions, CronjobSettingForm, CronjobTypes, SyncNow }
+export { CronjobSetting, CronjobSettingActions, CronjobSettingForm, CronjobTypes, SyncNow }
--- src/modules/scrapper/index.jsx
+++ src/modules/scrapper/index.jsx
@@ -1,82 +1,44 @@
 import { memo, useState } from 'react'
-import useSWR from 'swr'
 
-import { Button, EmptyTable, Loading } from '@components'
+import { Button } from '@components'
 
-import { CronjobSettingActions, CronjobSettingForm, SyncNow } from '@modules/scrapper/components'
-import { fetchCronjobSettings } from '@modules/scrapper/api'
+import { CronjobSetting } from '@modules/scrapper/components'
 
-import { can, convertFrom24To12Format } from '@utils/helpers'
-import { JOB_SOURCES, SETTING_HEADS } from '@constants/scrapper'
+import { UserIcon, ActionsIcons } from '@icons'
 
-import { CreateIcon } from '@icons'
-
-const Scrapper = () => {
-    const [setting, setSetting] = useState()
-    const [show, setShow] = useState(false)
-
-    const { data, isLoading, error, mutate } = useSWR('/api/job_scraper/scheduler/', fetchCronjobSettings)
-
-    const handleClick = (values = null) => {
-        setSetting(values)
-        setShow(true)
-    }
-
-    if (isLoading) return <Loading />
+const Profile = () => {
+    const [activeTab, setActiveTab] = useState({ setting: true, links: false })
 
     return (
-        <div className='max-w-full overflow-x-auto mb-14 px-5'>
-            <div className='flex items-center space-x-4 pb-6'>
-                {can('create_cronjob_setting') && (
-                    <Button label='Create Cronjob Setting' fit icon={CreateIcon} onClick={() => handleClick()} />
-                )}
-                {can('run_scrapper') && <SyncNow />}
-            </div>
-            <table className='table-auto w-full text-sm text-left text-[#048C8C]'>
-                <thead className='text-xs uppercase border border-[#048C8C]'>
-                    <tr>
-                        {SETTING_HEADS.map(heading => (
-                            <th scope='col' className='px-3 py-4' key={heading}>
-                                {heading}
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody className='bg-white'>
-                    {data?.settings?.length > 0 && !error ? (
-                        data?.settings?.map((row, idx) => (
-                            <tr className='border-b border-[#006366] border-opacity-30 hover:bg-gray-100' key={row.id}>
-                                <td className='px-3 py-6'>{idx + 1}</td>
-                                <td className='px-3 py-6'>{JOB_SOURCES[row?.job_source]}</td>
-                                <td className='px-3 py-6'>{row?.time_based ? 'Time Based' : 'Interval Based'}</td>
-                                <td className='px-3 py-6'>
-                                    {row?.time_based
-                                        ? convertFrom24To12Format(row?.time)
-                                        : row?.interval && row?.interval_type
-                                        ? `After every ${row?.interval} ${row?.interval_type}`
-                                        : 'not-specified'}
-                                </td>
-                                <td className='px-3 py-6 float-right'>
-                                    {can(['edit_cronjob_setting', 'delete_cronjob_setting']) && (
-                                        <CronjobSettingActions
-                                            id={row?.id}
-                                            edit={() => handleClick(row)}
-                                            mutate={mutate}
-                                        />
-                                    )}
-                                </td>
-                            </tr>
-                        ))
-                    ) : (
-                        <EmptyTable cols={6} msg='No cronjob settings found yet!' />
+        <div className='p-2'>
+            <div className='p-4 border border-[#71dfd0] rounded-lg shadow-md'>
+                <div className='flex flex-col mb-4'>
+                    <div className='flex flex-row mb-4 md:gap-5'>
+                        <Button
+                            label='Cronjob Settings'
+                            fit
+                            icon={UserIcon}
+                            classes={`md:pr-8 md:pl-6 ${!activeTab.setting && 'border-gray-200'}`}
+                            onClick={() => setActiveTab({ setting: true, links: false })}
+                        />
+                        <Button
+                            label='Job Source Links / URLs'
+                            fit
+                            icon={ActionsIcons}
+                            classes={`md:pr-8 md:pl-6 ${!activeTab.links && 'border-gray-200'}`}
+                            onClick={() => setActiveTab({ setting: false, links: true })}
+                        />
+                    </div>
+                    {activeTab.setting && (
+                        <div className='mt-5'>
+                            <CronjobSetting />
+                        </div>
                     )}
-                </tbody>
-            </table>
-            {can(['edit_cronjob_setting', 'create_cronjob_setting']) && show && (
-                <CronjobSettingForm show={show} setShow={setShow} mutate={mutate} setting={setting} />
-            )}
+                    {activeTab.links && <div className='p-2 mt-4'>Here comes Links section</div>}
+                </div>
+            </div>
         </div>
     )
 }
 
-export default memo(Scrapper)
+export default memo(Profile)
