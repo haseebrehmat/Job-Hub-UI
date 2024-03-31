6875b956342fc968f55537c10ba8ba553ab2832d
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-03-31 16:15:56 +0500

    feat: add companies dropdown on dashboard

--- src/modules/dashboard/index.jsx
+++ src/modules/dashboard/index.jsx
@@ -1,37 +1,69 @@
-import { memo, useState } from 'react'
+import { memo, useState, useMemo } from 'react'
 import useSWR from 'swr'
 
-import { Loading } from '@components'
+import { Loading, SelectBox } from '@components'
 
 import { Filters, Statistics, Leads, WarmLeads, TechStacks } from '@modules/dashboard/components'
 import { fetchDashboardData } from '@modules/dashboard/api'
-import { can, decodeJwt } from '@/utils/helpers'
+import { fetchCompanies } from '@modules/userManagement/api'
+
+import { can, decodeJwt, isSuper, parseComapnies, parseSelectedCompany } from '@/utils/helpers'
 
 const Dashboard = () => {
     const user = decodeJwt()
-    const [filters, setFilters] = useState({ from_date: '', to_date: '' })
-    const { data, error, isLoading } = useSWR([filters], () => fetchDashboardData(filters.from_date, filters.to_date))
+    const [filters, setFilters] = useState({ from_date: '', to_date: '', company: '' })
+    const { data, error, isLoading } = useSWR([filters], () => fetchDashboardData(filters), {
+        revalidateOnFocus: false,
+    })
+    const {
+        data: fetchedCompanies,
+        error: companyError,
+        isLoading: companyLoading,
+    } = useSWR(`/api/auth/company/`, isSuper() && fetchCompanies, {
+        revalidateOnFocus: false,
+    })
 
-    if (isLoading) return <Loading />
+    const memoizedCompaniesSelect = useMemo(
+        () =>
+            isSuper() &&
+            (companyLoading ? (
+                <small className='ml-1 p-3 text-xs text-gray-400'>Companies Loading...</small>
+            ) : companyError ? (
+                <div>Failed to load companies</div>
+            ) : (
+                <SelectBox
+                    options={parseComapnies(fetchedCompanies?.companies)}
+                    selected={parseSelectedCompany(filters.company, fetchedCompanies?.companies)}
+                    handleChange={({ value }) => setFilters({ ...filters, company: value })}
+                    classes='text-gray-500 text-sm w-1/4'
+                />
+            )),
+        [companyLoading, companyError, fetchedCompanies?.companies, filters.company]
+    )
+
+    if (isLoading || companyLoading) return <Loading />
 
     return user?.permissions?.length > 0 ? (
         <div className='flex flex-col w-full space-y-14'>
             {!error ? (
                 <>
-                    <Filters filters={filters} setFilters={setFilters} />
-                    {can('view_statistics') && (
+                    <div className='flex justify-between'>
+                        <Filters filters={filters} setFilters={setFilters} />
+                        {memoizedCompaniesSelect}
+                    </div>
+                    {can('view_statistics') && data?.statistics && (
                         <div className='block xl:hidden'>
                             <Statistics classes='grid grid-cols-3 space-y-4 space-x-3' data={data?.statistics} />
                         </div>
                     )}
                     <div className='flex items-start justify-between'>
                         <div className='flex flex-col w-4/5 space-y-16'>
-                            <Leads data={data?.leads} />
-                            <TechStacks data={data?.tech_jobs} />
-                            <WarmLeads data={data?.leads} />
+                            {data?.leads && data?.leads?.length > 0 && <Leads data={data?.leads} />}
+                            {data?.tech_jobs && data?.tech_jobs?.length > 0 && <TechStacks data={data?.tech_jobs} />}
+                            {data?.leads && data?.leads?.length > 0 && <WarmLeads data={data?.leads} />}
                         </div>
                         <div className='w-1/5 pl-6 invisible xl:visible'>
-                            {can('view_statistics') && (
+                            {can('view_statistics') && data?.statistics && (
                                 <Statistics classes='flex-col space-y-8' data={data?.statistics} />
                             )}
                         </div>
