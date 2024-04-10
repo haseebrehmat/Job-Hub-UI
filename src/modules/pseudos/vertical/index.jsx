096095458a9dd3d57656bebbd4748ac5c1512039
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-04-10 14:48:08 +0500

    feat: eduction section init

--- src/modules/pseudos/vertical/index.jsx
+++ src/modules/pseudos/vertical/index.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 
 import { Button } from '@components'
 
-import { BasicInfo, Skills, Experiences } from '@/modules/pseudos/vertical/sections'
+import { BasicInfo, Skills, Experiences, Education } from '@/modules/pseudos/vertical/sections'
 
 import { VERTICAL_INITIAL_TABS, VERTICAL_SECTIONS } from '@constants/pseudos'
 
@@ -34,7 +34,7 @@ const Vertical = () => {
                 {activeTab.basic && <BasicInfo id={id} />}
                 {activeTab.skill && <Skills id={id} />}
                 {activeTab.experience && <Experiences id={id} />}
-                {activeTab.education && <div className='mt-5'>here goes educations</div>}
+                {activeTab.education && <Education id={id} />}
                 {activeTab.hobby && <div className='mt-5'>here goes hobbys</div>}
                 {activeTab.language && <div className='mt-5'>here goes languages</div>}
                 {activeTab.summary && <div className='mt-5'>here goes summarys</div>}
--- src/modules/pseudos/vertical/sections.jsx
+++ src/modules/pseudos/vertical/sections.jsx
@@ -1,5 +1,6 @@
 import Skills from '@modules/pseudos/vertical/skills'
 import Basic from '@modules/pseudos/vertical/basic'
 import Experiences from '@modules/pseudos/vertical/experiences'
+import Education from '@modules/pseudos/vertical/education'
 
-export { Skills, Basic as BasicInfo, Experiences }
+export { Skills, Basic as BasicInfo, Experiences, Education }
