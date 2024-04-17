d1c5446f0daa4316a279adff67b41b79eb4de3c7
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-04-17 09:28:39 +0500

    fix: template with empty variables bug

--- src/modules/settings/resumeBuilder/template1.jsx
+++ src/modules/settings/resumeBuilder/template1.jsx
@@ -8,16 +8,16 @@ import { svgs } from './svgs'
 const Template1 = ({ data: dev }) => (
     <div className='p-8 bg-white shadow-2xl border-2 rounded-lg md:w-[24cm] md:max-h-[29.7cm] w-full h-full'>
         <div className='flex items-center'>
-            <img src={dev.basic.avatar ?? avatarPlaceholder} alt='Profile' className='w-28 h-w-28 rounded-full' />
+            <img src={dev?.basic?.avatar ?? avatarPlaceholder} alt='Profile' className='w-28 h-w-28 rounded-full' />
             <div className='flex items-center justify-between w-full'>
                 <div className='ml-4'>
-                    <h2 className='text-xl font-bold'>{dev.basic.name ?? 'no name'}</h2>
-                    <p className='text-gray-600'>{dev.basic.title ?? 'no title'}</p>
+                    <h2 className='text-xl font-bold'>{dev?.basic?.name ?? 'no name'}</h2>
+                    <p className='text-gray-600'>{dev?.basic?.title ?? 'no title'}</p>
                 </div>
                 <div className='ml-4 text-end'>
-                    <p className='text-sm py-0.5 text-gray-800'>{dev.basic.email ?? 'no email'}</p>
-                    <p className='text-sm py-0.5 text-gray-800'>{dev.basic.phone ?? 'no phone'}</p>
-                    <p className='text-sm py-0.5 text-gray-800'>{dev.basic.address ?? 'no address'}</p>
+                    <p className='text-sm py-0.5 text-gray-800'>{dev?.basic?.email ?? 'no email'}</p>
+                    <p className='text-sm py-0.5 text-gray-800'>{dev?.basic?.phone ?? 'no phone'}</p>
+                    <p className='text-sm py-0.5 text-gray-800'>{dev?.basic?.address ?? 'no address'}</p>
                 </div>
             </div>
         </div>
@@ -25,12 +25,12 @@ const Template1 = ({ data: dev }) => (
         <div className='grid grid-cols-2'>
             <div>
                 <h3 className='text-lg font-bold'>Summary</h3>
-                <p className='text-gray-600 text-sm'>{dev.summary ?? ''}</p>
+                <p className='text-gray-600 text-sm'>{dev?.summary ?? ''}</p>
             </div>
             <div className='ml-4'>
                 <h3 className='text-lg font-bold mb-2'>Skills</h3>
                 <div className='ml-4 space-y-1.5'>
-                    {dev.skills.map(({ name, level }, index) => (
+                    {dev?.skills?.map(({ name, level }, index) => (
                         <div className='flex items-center justify-between' key={index}>
                             <span>{name}</span>
                             <div className='flex space-x-3'>
@@ -57,11 +57,11 @@ const Template1 = ({ data: dev }) => (
         </div>
         <hr className='my-6 border-2' />
         <div className='grid grid-cols-2'>
-            {dev.experience.length > 0 && (
+            {dev?.experience?.length > 0 && (
                 <div>
                     <h3 className='text-lg font-bold'>Experience</h3>
                     <div className='flex flex-col'>
-                        {dev.experience.map(({ company, title, from, to, description }, index) => (
+                        {dev?.experience.map(({ company, title, from, to, description }, index) => (
                             <div className='ml-4 mt-4' key={index}>
                                 <h4 className='text-md font-bold'>{title}</h4>
                                 <p className='text-gray-600'>
@@ -73,11 +73,11 @@ const Template1 = ({ data: dev }) => (
                     </div>
                 </div>
             )}
-            {dev.education.length > 0 && (
+            {dev?.education?.length > 0 && (
                 <div className='ml-4'>
                     <h3 className='text-lg font-bold'>Education</h3>
                     <div className='flex flex-col'>
-                        {dev.education.map(({ institute, degree, from, to, grade }, index) => (
+                        {dev?.education.map(({ institute, degree, from, to, grade }, index) => (
                             <div className='ml-4 mt-4' key={index}>
                                 <h4 className='text-md font-bold'>{degree}</h4>
                                 <p className='text-gray-600'>
@@ -93,26 +93,26 @@ const Template1 = ({ data: dev }) => (
         <hr className='my-6 border-2' />
         <div className='grid grid-cols-2'>
             <div className='flex flex-col space-y-1'>
-                {dev.hobbies.length > 0 && (
+                {dev?.hobbies?.length > 0 && (
                     <div>
                         <h3 className='text-lg font-bold'>Hobbies</h3>
                         <div className='ml-4'>
-                            {dev.hobbies.map((name, index) => (
+                            {dev?.hobbies.map((name, index) => (
                                 <div className='inline-block' key={index}>
                                     <span className='pr-2'>{name}</span>
                                     <span className='pr-2 text-gray-400 text-2xl'>
-                                        {dev.hobbies.length !== index + 1 && '|'}
+                                        {dev?.hobbies.length !== index + 1 && '|'}
                                     </span>
                                 </div>
                             ))}
                         </div>
                     </div>
                 )}
-                {Object.entries(dev.links).length > 0 && (
+                {Object.entries(dev?.links)?.length > 0 && (
                     <div>
                         <h3 className='text-lg font-bold mt-4 mb-3'>Social Links</h3>
                         <div className='ml-4'>
-                            {Object.entries(dev.links).map(([name, url], index) => (
+                            {Object.entries(dev?.links).map(([name, url], index) => (
                                 <div className='inline-block mr-6 opacity-60' key={index}>
                                     <a href={url} target='_blank' className='w-10 h-10' rel='noreferrer'>
                                         {name in svgs ? svgs[name] : svgs.other}
@@ -124,11 +124,11 @@ const Template1 = ({ data: dev }) => (
                 )}
             </div>
             <div className='ml-4'>
-                {dev.languages.length > 0 && (
+                {dev?.languages?.length > 0 && (
                     <div>
                         <h3 className='text-lg font-bold mb-2'>Languages</h3>
                         <div className='ml-4 space-y-2'>
-                            {dev.languages.map(({ name, level }, index) => (
+                            {dev?.languages.map(({ name, level }, index) => (
                                 <div className='flex items-center justify-between' key={index}>
                                     <span>{name}</span>
                                     <div className='flex space-x-3'>
--- src/modules/settings/resumeBuilder/template2.jsx
+++ src/modules/settings/resumeBuilder/template2.jsx
@@ -7,25 +7,25 @@ import { svgs } from './svgs'
 const Template2 = ({ data: dev }) => (
     <div className='p-10 bg-white shadow-2xl border-2 rounded-lg md:w-[24cm] md:max-h-[29.7cm] w-full h-full'>
         <div className='flex flex-col mb-10'>
-            <p className='text-blue-900 text-6xl font-bold'>{dev.basic.name ?? 'no name'}</p>
-            <p className='text-gray-500 text-3xl font-bold mt-3'>{dev.basic.title ?? 'no title'}</p>
+            <p className='text-blue-900 text-6xl font-bold'>{dev?.basic?.name ?? 'no name'}</p>
+            <p className='text-gray-500 text-3xl font-bold mt-3'>{dev?.basic?.title ?? 'no title'}</p>
         </div>
-        {dev.summary.length > 0 && (
+        {dev?.summary?.length > 0 && (
             <div className='flex items-baseline justify-start mb-3 mt-10'>
                 <p className='text-blue-900 text-xl font-bold w-1/4'>Summary</p>
                 <div className='flex flex-col w-3/4'>
                     <hr className='mb-3 border border-blue-900' />
-                    <p className='text-gray-700 text-sm'>{dev.summary ?? 'no summary'}</p>
+                    <p className='text-gray-700 text-sm'>{dev?.summary ?? 'no summary'}</p>
                 </div>
             </div>
         )}
-        {dev.experience.length > 0 && (
+        {dev?.experience?.length > 0 && (
             <div className='flex items-baseline justify-start my-3'>
                 <p className='text-blue-900 text-xl font-bold w-1/4'>Experience</p>
                 <div className='flex flex-col w-3/4'>
                     <hr className='mb-3 border border-blue-900' />
                     <div className='space-y-5'>
-                        {dev.experience.map(({ company, title, from, to, description }, index) => (
+                        {dev?.experience.map(({ company, title, from, to, description }, index) => (
                             <div className='grid grid-cols-2 text-sm' key={index}>
                                 <div className='flex flex-col'>
                                     <p className='text-blue-900 font-bold'>{title}</p>
@@ -41,13 +41,13 @@ const Template2 = ({ data: dev }) => (
                 </div>
             </div>
         )}
-        {dev.education.length > 0 && (
+        {dev?.education?.length > 0 && (
             <div className='flex items-baseline justify-start my-3'>
                 <p className='text-blue-900 text-xl font-bold w-1/4'>Education</p>
                 <div className='flex flex-col w-3/4'>
                     <hr className='mb-3 border border-blue-900' />
                     <div className='space-y-5'>
-                        {dev.education.map(({ degree, from, to, institute, grade }, index) => (
+                        {dev?.education.map(({ degree, from, to, institute, grade }, index) => (
                             <div className='grid grid-cols-2 text-sm' key={index}>
                                 <div className='flex flex-col'>
                                     <p className='text-blue-900 font-bold'>{institute}</p>
@@ -65,17 +65,17 @@ const Template2 = ({ data: dev }) => (
                 </div>
             </div>
         )}
-        {dev.skills.length > 0 && (
+        {dev?.skills?.length > 0 && (
             <div className='flex items-baseline justify-start my-3'>
                 <p className='text-blue-900 text-xl font-bold w-1/4'>Skills</p>
                 <div className='flex flex-col w-3/4'>
                     <hr className='mb-3 border border-blue-900' />
                     <div>
-                        {dev.skills.map(({ name }, index) => (
+                        {dev?.skills.map(({ name }, index) => (
                             <div className='inline-block' key={index}>
                                 <span className='pr-4'>{name}</span>
                                 <span className='pr-4 text-gray-400 text-lg'>
-                                    {dev.skills.length !== index + 1 && '|'}
+                                    {dev?.skills.length !== index + 1 && '|'}
                                 </span>
                             </div>
                         ))}
@@ -90,19 +90,19 @@ const Template2 = ({ data: dev }) => (
                 <div className='grid grid-cols-2 mb-3 text-sm'>
                     <div className='flex gap-4 my-3'>
                         <span>{svgs.phone}</span>
-                        <p className='text-gray-700'>{dev.basic.phone ?? 'no phone'}</p>
+                        <p className='text-gray-700'>{dev?.basic?.phone ?? 'no phone'}</p>
                     </div>
                     <div className='flex gap-4 my-3'>
                         <span>{svgs.portfolio}</span>
-                        <p className='text-gray-700'>{dev.basic.portfolio ?? 'no portfolio'}</p>
+                        <p className='text-gray-700'>{dev?.basic?.portfolio ?? 'no portfolio'}</p>
                     </div>
                     <div className='flex gap-4 my-3'>
                         <span>{svgs.gmail}</span>
-                        <p className='text-gray-700'>{dev.basic.email ?? 'no email'}</p>
+                        <p className='text-gray-700'>{dev?.basic?.email ?? 'no email'}</p>
                     </div>
                     <div className='flex gap-4 my-3'>
                         <span>{svgs.address}</span>
-                        <p className='text-gray-700'>{dev.basic.address ?? 'no address'}</p>
+                        <p className='text-gray-700'>{dev?.basic?.address ?? 'no address'}</p>
                     </div>
                 </div>
             </div>
