1446c06c09f704ad44cce804f7c6148b172e1534
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-04-26 13:02:05 +0500

    fix: resume preview issue & cover letter updation

--- src/modules/pseudos/vertical/resumePreview/index.jsx
+++ src/modules/pseudos/vertical/resumePreview/index.jsx
@@ -14,34 +14,38 @@ const ResumeBuilder = ({ id }) => {
     if (isLoading) return <Loading />
     return (
         <div className='flex flex-col'>
-            <div className='flex flex-col items-center'>
-                <div className='flex flex-row mb-4 gap-5'>
-                    <Button
-                        label='Template 1'
-                        fit
-                        fill={tab === 1}
-                        classes={`md:px-6 rounded-none ${tab !== 1 && 'border-gray-200'}`}
-                        onClick={() => setTab(1)}
-                    />
-                    <Button
-                        label='Template 2'
-                        fit
-                        fill={tab === 2}
-                        classes={`md:px-6 rounded-none ${tab !== 2 && 'border-gray-200'}`}
-                        onClick={() => setTab(2)}
-                    />
+            {data ? (
+                <div className='flex flex-col items-center'>
+                    <div className='flex flex-row mb-4 gap-5'>
+                        <Button
+                            label='Template 1'
+                            fit
+                            fill={tab === 1}
+                            classes={`md:px-6 rounded-none ${tab !== 1 && 'border-gray-200'}`}
+                            onClick={() => setTab(1)}
+                        />
+                        <Button
+                            label='Template 2'
+                            fit
+                            fill={tab === 2}
+                            classes={`md:px-6 rounded-none ${tab !== 2 && 'border-gray-200'}`}
+                            onClick={() => setTab(2)}
+                        />
+                    </div>
+                    {tab === 1 && (
+                        <div className='p-8 bg-white shadow-2xl border-2 rounded-lg md:w-[21cm] md:min-h-[29.7cm] w-full h-full'>
+                            <Template1 data={data} />
+                        </div>
+                    )}
+                    {tab === 2 && (
+                        <div className='p-10 bg-white shadow-2xl border-2 rounded-lg md:w-[21cm] md:min-h-[29.7cm] w-full h-full'>
+                            <Template2 data={data} />
+                        </div>
+                    )}
                 </div>
-                {tab === 1 && (
-                    <div className='p-8 bg-white shadow-2xl border-2 rounded-lg md:w-[21cm] md:min-h-[29.7cm] w-full h-full'>
-                        <Template1 data={data} />
-                    </div>
-                )}
-                {tab === 2 && (
-                    <div className='p-10 bg-white shadow-2xl border-2 rounded-lg md:w-[21cm] md:min-h-[29.7cm] w-full h-full'>
-                        <Template2 data={data} />
-                    </div>
-                )}
-            </div>
+            ) : (
+                <span>Error to load resume previews</span>
+            )}
             <div className='bg-white px-4 mt-4 pt-4'>
                 <ActionButtons mutate={mutate} />
             </div>
