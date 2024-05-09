0647d319570f5611e238b9841174944b79d7e6e8
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-05-09 14:49:26 +0500

    fix: implemented upload manual resume frontend for apply job

--- src/modules/jobsFilter/applyForJob/index.jsx
+++ src/modules/jobsFilter/applyForJob/index.jsx
@@ -20,6 +20,13 @@ const ApplyForJob = () => {
     const [verticalId, setVerticalId] = useState(null)
     const [coverLetter, setCoverLetter] = useState(`Here your cover letter ${id}`)
 
+    const [resumeType, setResumeType] = useState(null)
+    const uploadResumeOptions = [
+        { label: 'Choose resume from templates', value: 'automatic' },
+        { label: 'Upload resume manually', value: 'manual' },
+    ]
+
+    const [resumePDF, setResumePDF] = useState('')
     const { data: data1, isLoading: isLoading1 } = useSWRImmutable(
         `/api/profile/user_vertical/?user_id=${user_id}`,
         fetchUserVerticals
@@ -32,12 +39,30 @@ const ApplyForJob = () => {
     const { values, wait, handleSubmit, trigger, setFieldValue } = useMutate(
         '/api/job_portal/job_status/',
         applyJob,
-        { status: 1, vertical_id: verticalId, job: id, resume: pdfBlob },
+        {
+            status: 1,
+            vertical_id: verticalId,
+            job: id,
+            resume: pdfBlob,
+            resume_type: resumeType?.value,
+        },
         null,
-        async formValues => trigger({ ...formValues, cover_letter: coverLetter }),
+        async formValues => {
+            trigger({
+                ...formValues,
+                cover_letter: coverLetter,
+                resume:
+                    resumeType?.value === 'automatic'
+                        ? pdfBlob
+                        : resumeType?.value === 'manual' && resumePDF
+                        ? resumePDF
+                        : null,
+            })
+        },
         null,
         () => redirect('/jobs-portal')
     )
+
     useMemo(() => setCoverLetter(data2?.cover_letter), [data2])
 
     if (isLoading1 || isLoading2 || wait) return <Loading />
@@ -57,6 +82,19 @@ const ApplyForJob = () => {
                                 }}
                                 placeholder='Select vertical'
                             />
+                            {verticalId && (
+                                <>
+                                    <p className='italic text-gray-600 py-2'>Resume Type</p>
+                                    <CustomSelector
+                                        options={uploadResumeOptions}
+                                        placeholder='Select Resume Type'
+                                        handleChange={value => {
+                                            setResumeType(value)
+                                        }}
+                                        required
+                                    />
+                                </>
+                            )}
                         </div>
                         {verticalId && (
                             <>
@@ -65,12 +103,39 @@ const ApplyForJob = () => {
                                     value={coverLetter}
                                     onChange={text => setCoverLetter(text)}
                                 />
-                                <Button label='Apply' type='submit' fill fit classes='px-8 md:px-12 !rounded-full' />
+                                {resumeType && (
+                                    <Button
+                                        label='Apply'
+                                        type='submit'
+                                        fill
+                                        fit
+                                        classes='px-8 md:px-12 !rounded-full'
+                                    />
+                                )}{' '}
                             </>
                         )}
                     </div>
                     <div className='xs:w-1/2'>
-                        {verticalId && <ResumeSelect vertical={verticalId} setResume={setPdfBlob} />}
+                        {verticalId &&
+                            resumeType &&
+                            (resumeType?.value === 'automatic' ? (
+                                <ResumeSelect vertical={verticalId} setResume={setPdfBlob} />
+                            ) : (
+                                <div className='bg-[#edfdfb] p-5 border border-gray-200 text-center rounded-lg md:mt-8 border-1 text-gray-600'>
+                                    <p>Upload Resume in PDF</p>
+                                    <p className='my-3'>
+                                        <input
+                                            type='file'
+                                            accept='.pdf'
+                                            name='resume'
+                                            onChange={e => {
+                                                setResumePDF(e.target.files[0])
+                                            }}
+                                            required
+                                        />
+                                    </p>
+                                </div>
+                            ))}
                     </div>
                 </div>
             </form>
