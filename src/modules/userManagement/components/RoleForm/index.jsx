1a93559ecc77f1848c7a931468e5ab45852157c9
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-05-12 12:44:12 +0500

    feat: update permissions module and perms of phase

--- src/modules/userManagement/components/RoleForm/index.jsx
+++ src/modules/userManagement/components/RoleForm/index.jsx
@@ -1,17 +1,15 @@
 import { memo, useState } from 'react'
 import { toast } from 'react-hot-toast'
 
-import { useMutate, useDelete } from '@/hooks'
+import { useMutate } from '@/hooks'
 
-import { Button, Drawer, Input, CustomDilog } from '@components'
+import { Button, Drawer, Input } from '@components'
+
 import { Permissions } from '@modules/userManagement/components'
 import { saveRole } from '@modules/userManagement/api'
 
 import { roleSchema } from '@utils/schemas'
 import { getMsg } from '@utils/helpers'
-import { can } from '@/utils/helpers'
-
-import { TrashIcon } from '@icons'
 
 const RoleForm = ({ show, setShow, mutate, role }) => {
     const [permissions, setPermissions] = useState(role?.permissions?.map(p => p.codename) ?? [])
@@ -28,26 +26,12 @@ const RoleForm = ({ show, setShow, mutate, role }) => {
         }
     )
 
-    const { wait, confirm } = useDelete(`/api/auth/role/${role?.id}/`)
-
-    const deleteRole = async () => {
-        const result = await confirm()
-        if (result && wait === false) {
-            setShow(false)
-            mutate('/api/auth/role/')
-        }
-    }
-
-    const { CustomModal, openModal } = CustomDilog(
-        'Confirm delete?',
-        `Are you sure want to delete the role: ${role?.name}`,
-        deleteRole
-    )
+    // const { wait, confirm } = useDelete(`/api/auth/role/${role?.id}/`)
 
     return (
-        <Drawer show={show} setShow={setShow} w='500px'>
+        <Drawer show={show} setShow={setShow} w='700px'>
             <form onSubmit={handleSubmit}>
-                <div className='grid grid-flow-row gap-2'>
+                <div className='grid grid-flow-row gap-2 overflow-y-auto'>
                     <p className='font-medium text-xl'>{role?.id ? 'Edit' : 'Create'} Role</p>
                     <hr className='mb-2' />
                     <span className='text-xs font-semibold'>Name*</span>
@@ -57,17 +41,6 @@ const RoleForm = ({ show, setShow, mutate, role }) => {
                     <div className='pt-2 grid grid-cols-3 gap-3'>
                         <Button label={role?.id ? 'Update' : 'Submit'} type='submit' fill />
                         <Button label='Cancel' onClick={() => setShow(false)} />
-
-                        {/* {CustomModal}
-
-                        {can('delete_role') && role?.id && (
-                            <Button
-                                label='Delete'
-                                classes='bg-transparent text-red-500 border-red-500'
-                                icon={TrashIcon}
-                                onClick={openModal}
-                            />
-                        )} */}
                     </div>
                 </div>
             </form>
