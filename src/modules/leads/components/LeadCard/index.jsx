5de8ba9d6ac8dd9009925bd064580a49d605c021
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-05-23 19:15:46 +0500

    fix: vertical in lead card

--- src/modules/leads/components/LeadCard/index.jsx
+++ src/modules/leads/components/LeadCard/index.jsx
@@ -2,22 +2,26 @@ import { memo } from 'react'
 
 import { Badge } from '@components'
 
-import { CurrentPhaseIcon } from '@icons'
+import { CurrentPhaseIcon, LeadVerticalIcon } from '@icons'
 
 const LeadCard = ({ lead, dispatch }) => (
     <div
         className='bg-white text-gray-500 border border-[#048C8C] rounded-md p-2'
         onClick={() => dispatch({ show: true, draggable: lead?.id })}
     >
-        <h2 className='text-sm capitalize'>{lead?.applied_job?.title}</h2>
-        <h2 className='capitalize italic py-2'>{lead?.applied_job?.company}</h2>
-        <h2 className='flex'>
+        <p className='text-sm capitalize'>{lead?.applied_job?.title}</p>
+        <p className='capitalize italic py-2'>{lead?.applied_job?.company}</p>
+        <p className='flex space-x-1 mb-2'>
+            <span>{LeadVerticalIcon}</span>
+            <span className='text-xs capitalize'>{lead?.applied_job?.vertical_name ?? 'N/A'}</span>
+        </p>
+        <p className='flex'>
             <span>{CurrentPhaseIcon}</span>
             <span className='text-xs'>{lead?.phase_name ?? 'not set'}</span>
-        </h2>
-        <h2 className='text-right'>
+        </p>
+        <p className='text-right'>
             <Badge label={lead?.applied_job?.tech_stack} />
-        </h2>
+        </p>
     </div>
 )
 
