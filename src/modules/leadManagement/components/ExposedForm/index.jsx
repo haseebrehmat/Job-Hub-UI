39590491339097aaffc7a05dc5efa44526a55a7f
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-05-30 18:13:52 +0500

    fix: selected companies reset issue when candidate is added or removed

--- src/modules/leadManagement/components/ExposedForm/index.jsx
+++ src/modules/leadManagement/components/ExposedForm/index.jsx
@@ -1,4 +1,4 @@
-import { memo } from 'react'
+import { memo, useState } from 'react'
 import useSWR from 'swr'
 
 import { useMutate } from '@/hooks'
@@ -8,18 +8,17 @@ import { Button, CustomSelector } from '@components'
 import { fetchCompanies } from '@modules/userManagement/api'
 import { saveDesignation } from '@modules/leadManagement/api'
 
-import { designationSchema } from '@utils/schemas'
 import { parseComapnies } from '@utils/helpers'
 
 const ExposedForm = ({ mutate, candidates = [], dispatch = null }) => {
+    const [selectedCompanies, setSelectedCompanies] = useState([])
     const { data, isLoading, error } = useSWR('/api/auth/company/', fetchCompanies)
-
-    const { values, errors, handleSubmit, trigger, setFieldValue } = useMutate(
+    const { handleSubmit, trigger } = useMutate(
         `/api/candidate_management/designation/`,
         saveDesignation,
-        { companies: [], candidates },
-        designationSchema,
-        async formValues => trigger({ ...formValues }),
+        { companies: selectedCompanies, candidates },
+        null,
+        async formValues => trigger({ ...formValues, companies: selectedCompanies.map(({ value }) => value) }),
         null,
         () => {
             mutate()
@@ -38,14 +37,13 @@ const ExposedForm = ({ mutate, candidates = [], dispatch = null }) => {
                     <CustomSelector
                         name='members'
                         options={parseComapnies(data?.companies)}
-                        handleChange={obj => setFieldValue('companies', obj)}
-                        selectorValue={values.companies}
+                        handleChange={obj => setSelectedCompanies(obj)}
+                        selectorValue={selectedCompanies}
                         isMulti
                         placeholder='Select companies'
                     />
-                    {errors.companies && <small className='__error'>{errors.companies}</small>}
                 </div>
-                {values.companies.length > 0 && candidates.length > 0 && (
+                {selectedCompanies.length > 0 && candidates.length > 0 && (
                     <>
                         <Button label='Expose' type='submit' fill fit />
                         <Button label='Clear' fit onClick={() => dispatch({ ids: [] })} />
