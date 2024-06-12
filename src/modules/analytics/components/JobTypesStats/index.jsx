6294b6394c3f3009f995ad0f1378c77f6b40525b
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-06-12 18:36:28 +0500

    feat: stats empty value placeholder

--- src/modules/analytics/components/JobTypesStats/index.jsx
+++ src/modules/analytics/components/JobTypesStats/index.jsx
@@ -5,31 +5,35 @@ import { formatNum } from '@utils/helpers'
 import { JOB_TYPE_NUMBER_STYLE } from '@constants/analytics'
 
 const JobTypesStats = ({ data = [] }) => (
-    <div className='border px-2 pt-10 pb-4 text-[#1E6570] relative'>
+    <div className='border px-2 pt-10 pb-4 text-[#1E6570] mt-10 relative'>
         <p className='-mt-16 absolute px-2 py-1.5 border bg-[#EDFDFB] text-lg tracking-widest'>Job Types Analytics</p>
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4'>
-            {data?.map((d, index) => (
-                <div
-                    className='border shadow-md pl-2 pr-3 py-2 bg-[#EDFDFB] w-full h-28 flex items-center justify-center text-center'
-                    key={index}
-                >
-                    <div className='flex items-center justify-between'>
-                        <div className='flex flex-col'>
-                            <AnimatedNumber2
-                                initialValue={0}
-                                component='p'
-                                value={d.value}
-                                stepPrecision={0}
-                                style={JOB_TYPE_NUMBER_STYLE}
-                                duration={1000}
-                                formatValue={n => formatNum(n)}
-                            />
-                            <p className='uppercase text-xs tracking-widest'>{d.name}</p>
+        {data?.length > 0 ? (
+            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4'>
+                {data?.map((d, index) => (
+                    <div
+                        className='border shadow-md pl-2 pr-3 py-2 bg-[#EDFDFB] w-full h-28 flex items-center justify-center text-center'
+                        key={index}
+                    >
+                        <div className='flex items-center justify-between'>
+                            <div className='flex flex-col'>
+                                <AnimatedNumber2
+                                    initialValue={0}
+                                    component='p'
+                                    value={d.value}
+                                    stepPrecision={0}
+                                    style={JOB_TYPE_NUMBER_STYLE}
+                                    duration={1000}
+                                    formatValue={n => formatNum(n)}
+                                />
+                                <p className='uppercase text-xs tracking-widest'>{d.name}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
+        ) : (
+            <span className='italic ml-2'>No statics found yet</span>
+        )}
     </div>
 )
 
--- src/modules/analytics/components/TechStackStats/index.jsx
+++ src/modules/analytics/components/TechStackStats/index.jsx
@@ -7,26 +7,30 @@ import { TECH_STACK_NUMBER_STYLE } from '@constants/analytics'
 const TechStacksStats = ({ data = [] }) => (
     <div className='border px-2 pt-10 pb-4 text-[#1E6570] mt-10 relative'>
         <p className='-mt-16 absolute px-2 py-1.5 border bg-[#EDFDFB] text-lg tracking-widest'>Tech Stacks Analytics</p>
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4'>
-            {data?.map((d, index) => (
-                <div className='border shadow-md pl-2 pr-3 py-2 bg-[#EDFDFB] w-full' key={index}>
-                    <div className='flex items-center justify-between'>
-                        <div className='flex flex-col'>
-                            <AnimatedNumber2
-                                initialValue={0}
-                                component='p'
-                                value={d.value}
-                                stepPrecision={0}
-                                style={TECH_STACK_NUMBER_STYLE}
-                                duration={1000}
-                                formatValue={n => formatNum(n)}
-                            />
-                            <p className='uppercase text-xs tracking-widest'>{d.name}</p>
+        {data?.length > 0 ? (
+            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4'>
+                {data?.map((d, index) => (
+                    <div className='border shadow-md pl-2 pr-3 py-2 bg-[#EDFDFB] w-full' key={index}>
+                        <div className='flex items-center justify-between'>
+                            <div className='flex flex-col'>
+                                <AnimatedNumber2
+                                    initialValue={0}
+                                    component='p'
+                                    value={d.value}
+                                    stepPrecision={0}
+                                    style={TECH_STACK_NUMBER_STYLE}
+                                    duration={1000}
+                                    formatValue={n => formatNum(n)}
+                                />
+                                <p className='uppercase text-xs tracking-widest'>{d.name}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
+        ) : (
+            <span className='italic ml-2'>No statics found yet</span>
+        )}
     </div>
 )
 
