2d21cec0b927c12438e94bca04769146412f042f
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-06-15 20:50:43 +0500

    update: remove total from abbreviations

--- src/modules/analytics/components/TechStackPies/index.jsx
+++ src/modules/analytics/components/TechStackPies/index.jsx
@@ -47,18 +47,20 @@ const TechStackPies = ({ data, stack = null }) => {
                 </ResponsiveContainer>
                 <p className='text-center text-lg font-bold uppercase tracking-wider'>{stack ?? 'No stack'}</p>
                 <div className='grid grid-cols-2 p-4 mt-6'>
-                    {Object.keys(JOB_TYPES).map(key => (
-                        <div className='flex items-center' key={key}>
-                            <span>
-                                {transformPascal(JOB_TYPES[key])
-                                    .split(' ')
-                                    .map(word => word.charAt(0).toUpperCase())
-                                    .join('')}
-                            </span>
-                            <span className='mx-2'>{BreadIcon}</span>
-                            <span className='text-sm'>{transformPascal(JOB_TYPES[key])}</span>
-                        </div>
-                    ))}
+                    {Object.keys(JOB_TYPES)
+                        .slice(0, Object.keys(JOB_TYPES).length - 1)
+                        .map(key => (
+                            <div className='flex items-center' key={key}>
+                                <span>
+                                    {transformPascal(JOB_TYPES[key])
+                                        .split(' ')
+                                        .map(word => word.charAt(0).toUpperCase())
+                                        .join('')}
+                                </span>
+                                <span className='mx-2'>{BreadIcon}</span>
+                                <span className='text-sm'>{transformPascal(JOB_TYPES[key])}</span>
+                            </div>
+                        ))}
                 </div>
             </div>
         </div>
