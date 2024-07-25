1d8e38520471bf2498a6c6a2fd9eab4b94f41d7d
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-07-25 18:35:44 +0500

    update: hide update & apply button

--- src/modules/jobsFilter/components/EditFormButtons/index.jsx
+++ src/modules/jobsFilter/components/EditFormButtons/index.jsx
@@ -6,13 +6,13 @@ const EditFormButtons = ({ wait, set }) => (
     <div className='pt-4 flex justify-end gap-2'>
         <Button label='Cancel' fit onClick={() => set({ show: false })} classes='!px-4' />
         <Button label='Update' fit fill type='submit' disabled={wait} classes='!px-8' />
-        <Button
+        {/* <Button
             label='Update & Apply'
             fit
             type='submit'
             disabled={wait}
             classes='!px-8 bg-gray-100 border-gray-400 text-gray-800 hover:bg-gray-100 hover:!text-[#048c8c] hover:!border-[#048c8c]'
-        />
+        /> */}
     </div>
 )
 
