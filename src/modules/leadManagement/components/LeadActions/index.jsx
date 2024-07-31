d1c8af144f1e6d6356b23a032885e315509bac48
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-07-31 21:06:50 +0500

    fix: remove leads action permissions

--- src/modules/leadManagement/components/LeadActions/index.jsx
+++ src/modules/leadManagement/components/LeadActions/index.jsx
@@ -3,28 +3,22 @@ import { Link } from 'react-router-dom'
 
 import { Button, Tooltip } from '@components'
 
-import { can } from '@utils/helpers'
-
 import { SeePassIcon, AssignCandidateIcon } from '@icons'
 
 const LeadActions = ({ lead, dispatch = null }) => (
-    <div className='absolute top-0 right-0 pl-2 mr-1 flex'>
-        {can('edit_designation') && (
-            <Tooltip text='Assign Candidate'>
-                <Link to={`/assign-candidate/${lead?.id}`} state={{ candidate: lead?.candidate?.id }}>
-                    {AssignCandidateIcon}
-                </Link>
-            </Tooltip>
-        )}
-        {can('delete_designation') && (
-            <Tooltip text='View lead Details'>
-                <Button
-                    classes='_icon-btn'
-                    icon={SeePassIcon}
-                    onClick={() => dispatch({ draggable: lead?.id, show: true })}
-                />
-            </Tooltip>
-        )}
+    <div className='pl-2 mr-1 flex'>
+        <Tooltip text='Assign Candidate'>
+            <Link to={`/assign-candidate/${lead?.id}`} state={{ candidate: lead?.candidate?.id }}>
+                {AssignCandidateIcon}
+            </Link>
+        </Tooltip>
+        <Tooltip text='View lead Details'>
+            <Button
+                classes='_icon-btn'
+                icon={SeePassIcon}
+                onClick={() => dispatch({ draggable: lead?.id, show: true })}
+            />
+        </Tooltip>
     </div>
 )
 
