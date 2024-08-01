20ea03684e74528d1310b6e1ac2ff77cce550bb0
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-08-01 18:31:39 +0500

    fix: Fix the issue in allow restrict user

--- src/modules/leadManagement/api/index.js
+++ src/modules/leadManagement/api/index.js
@@ -113,14 +113,7 @@ export const assignCandidate = (url, { arg: candidate }) =>
         .then(({ data }) => toast.success(data.detail || 'Candidate is assigned / reassigned successfully'))
 
 export const allowCandidateForLeads = (url, { arg: candidate }) =>
-    rawHttp
-        .post(url, candidate)
-        .then(({ data }) => toast.success(data.detail || 'Leads for candidate are allowed / denied successfully'))
-
-export const allowCandidateForLogin = (url, { arg: candidate }) =>
-    rawHttp
-        .post(url, candidate)
-        .then(({ data }) => toast.success(data.detail || 'Candidate are allowed / restricted to login successfully'))
+    rawHttp.post(url, candidate).then(({ data }) => toast.success(data.detail))
 
 export const fetchMyProfile = url =>
     http.get(url).then(({ data }) => ({ candidates: data?.candidate, regions: data?.all_regions }))
--- src/modules/leadManagement/components/CandidateActions/index.jsx
+++ src/modules/leadManagement/components/CandidateActions/index.jsx
@@ -1,10 +1,10 @@
 import { memo, useState } from 'react'
 
 import { useMutate } from '@/hooks'
-import { Button, DeleteDialog, Tooltip, Modal } from '@components'
+import { Button, DeleteDialog, Tooltip } from '@components'
 
-import { allowCandidateForLeads, allowCandidateForLogin } from '@modules/leadManagement/api'
-
+import { allowCandidateForLeads } from '@modules/leadManagement/api'
+import { CandidateActionsModel } from '@modules/leadManagement/components'
 import { can, decodeJwt } from '@utils/helpers'
 import { CANDIDATE_DELETION } from '@constants/allowDeletion'
 
@@ -14,24 +14,14 @@ const CandidateActions = ({ row, edit, mutate }) => {
     const user = decodeJwt()
     const [show, setShow] = useState(false)
     const [statusUpdate, setStatusUpdate] = useState(false)
+    const [loginStatusUpdate, setLoginStatusUpdate] = useState(false)
 
     const { handleSubmit, trigger } = useMutate(
         '/api/candidate_management/candidate/',
         allowCandidateForLeads,
-        { status: !row?.allowed_status, candidate: row?.id },
+        statusUpdate ? { status: !row?.allowed_status } : { login_status: !row?.allowed_login },
         null,
-        async formValues => trigger({ ...formValues }),
-        null,
-        () => mutate()
-    )
-    const [loginStatusUpdate, setLoginStatusUpdate] = useState(false)
-
-    const { handleSubmit: handleLoginSubmit, trigger: logintrigger } = useMutate(
-        '/api/candidate_management/candidate/',
-        allowCandidateForLogin,
-        { login_status: !row?.allowed_login, candidate: row?.id },
-        null,
-        async formValues => logintrigger({ ...formValues }),
+        async formValues => trigger({ ...formValues, candidate: row?.id }),
         null,
         () => mutate()
     )
@@ -74,53 +64,21 @@ const CandidateActions = ({ row, edit, mutate }) => {
                     <Button classes='_icon-btn' icon={SeePassIcon} onClick={() => setStatusUpdate(true)} />
                 </Tooltip>
             )}
-            <Modal
-                classes='!w-1/3'
-                show={statusUpdate}
-                setShow={setStatusUpdate}
-                content={
-                    <div className='w-full'>
-                        <h3 className='mt-1'>
+            <CandidateActionsModel
+                show={statusUpdate || loginStatusUpdate}
+                setShow={statusUpdate ? setStatusUpdate : setLoginStatusUpdate}
+                handleSubmit={handleSubmit}
+                message={
+                    statusUpdate ? (
+                        <>
                             Are you sure to {row?.allowed_status ? 'deny' : 'allow'} this candidate
                             <span className='font-bold mx-2'>{row?.name}</span>to take leads?
-                        </h3>
-                        <div className='flex items-center mt-3 gap-3 float-right'>
-                            <Button
-                                classes='bg-red-500 border-red-500 hover:bg-red-600'
-                                label='Confirm'
-                                fill
-                                onClick={() => {
-                                    handleSubmit()
-                                    setStatusUpdate(false)
-                                }}
-                            />
-                            <Button label='Cancel' onClick={() => setStatusUpdate(false)} />
-                        </div>
-                    </div>
-                }
-            />
-            <Modal
-                classes='!w-1/3'
-                show={loginStatusUpdate}
-                setShow={setLoginStatusUpdate}
-                content={
-                    <div className='w-full'>
-                        <h3 className='mt-1'>
-                            Are you sure to {row?.allowed_login ? 'allow' : 'restrict'} login for <b>{row?.name}</b> ?
-                        </h3>
-                        <div className='flex items-center mt-3 gap-3 float-right'>
-                            <Button
-                                classes='bg-red-500 border-red-500 hover:bg-red-600'
-                                label='Confirm'
-                                fill
-                                onClick={() => {
-                                    handleLoginSubmit()
-                                    setLoginStatusUpdate(false)
-                                }}
-                            />
-                            <Button label='Cancel' onClick={() => setLoginStatusUpdate(false)} />
-                        </div>
-                    </div>
+                        </>
+                    ) : (
+                        <>
+                            Are you sure to {row?.allowed_login ? 'allow' : 'restrict'} login for <b>{row?.name}</b>
+                        </>
+                    )
                 }
             />
         </div>
