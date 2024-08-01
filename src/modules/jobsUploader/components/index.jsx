9a39339454b150df13afb95e8596297f96de019c
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-08-01 15:56:15 +0500

    feat: wip expired jobs

--- src/modules/jobsUploader/components/index.jsx
+++ src/modules/jobsUploader/components/index.jsx
@@ -4,5 +4,6 @@ import JobActions from '@modules/jobsUploader/components/JobActions'
 import ManualJobs from '@modules/jobsUploader/components/ManualJobs'
 import CreateFormButtons from '@modules/jobsUploader/components/CreateFormButtons'
 import ManualJobActions from '@modules/jobsUploader/components/ManualJobActions'
+import ExpiredJobs from '@modules/jobsUploader/components/ExpiredJobs'
 
-export { FilesUploader, CreateJobForm, JobActions, ManualJobs, CreateFormButtons, ManualJobActions }
+export { FilesUploader, CreateJobForm, JobActions, ManualJobs, CreateFormButtons, ManualJobActions, ExpiredJobs }
