7e4f2d1f261c8174eb340ff91e24345d00d89f30
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-08-03 15:17:49 +0500

    feat: reset subfilters in analytics

--- src/modules/analytics/components/SubFilters/index.jsx
+++ src/modules/analytics/components/SubFilters/index.jsx
@@ -2,6 +2,8 @@ import { memo } from 'react'
 
 import { Button } from '@components'
 
+import { CrossIcon } from '@icons'
+
 const SubFilters = ({ options = null, vals = null, set = null }) =>
     options && (
         <div className='flex mt-4 gap-3 border text-[#1E6570] p-4 flex-wrap'>
@@ -26,6 +28,15 @@ const SubFilters = ({ options = null, vals = null, set = null }) =>
                           fill={vals.week === value}
                       />
                   ))}
+            {(vals?.tab === 'quarterly' && vals.month) || (vals?.tab === 'monthly' && vals.week) ? (
+                <Button
+                    label='reset'
+                    fit
+                    icon={CrossIcon}
+                    classes='!rounded-full !px-2 !py-1'
+                    onClick={() => (vals?.tab === 'quarterly' ? set({ month: '' }) : set({ week: '' }))}
+                />
+            ) : null}
         </div>
     )
 
