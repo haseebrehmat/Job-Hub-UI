55e13dd463adb2e82698d141db8ba12d29b01ffc
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-08-18 23:44:42 +0500

    feat: add emojis to note edit

--- src/modules/leadManagement/components/NoteEditForm/index.jsx
+++ src/modules/leadManagement/components/NoteEditForm/index.jsx
@@ -1,8 +1,10 @@
-import { memo, useMemo } from 'react'
+import { memo, useMemo, useState } from 'react'
 
 import { Textarea, Button } from '@components'
+import { EMOJIS } from '@constants/leadManagement'
 
 const NoteEditForm = ({ note, handleSubmit, setNote }) => {
+    const [showEmo, setShowEmo] = useState(false)
     const handleEditChange = e => setNote({ edit: e.target.value })
 
     const memoized = useMemo(
@@ -19,7 +21,29 @@ const NoteEditForm = ({ note, handleSubmit, setNote }) => {
         ),
         [note]
     )
-    return memoized
+    return (
+        <>
+            {memoized}
+            <div className='relative w-fit -mt-8'>
+                <span className='border-2 rounded-2xl px-1 py-1.5 cursor-pointer' onClick={() => setShowEmo(!showEmo)}>
+                    {EMOJIS[0]}
+                </span>
+                {showEmo && (
+                    <div className='absolute top-9 left-0 z-10 flex flex-wrap items-center gap-3 border rounded-3xl w-72 p-3 bg-white shadow-xl  border-slate-300'>
+                        {EMOJIS.map(emoji => (
+                            <span
+                                onClick={() => setNote({ edit: `${note?.edit} ${emoji}` })}
+                                className='cursor-pointer'
+                                key={emoji}
+                            >
+                                {emoji}
+                            </span>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </>
+    )
 }
 
 export default memo(NoteEditForm)
