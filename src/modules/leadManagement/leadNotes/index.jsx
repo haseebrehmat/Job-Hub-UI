14e522fb92c56f58c448c9c784944e2bede75b42
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-08-18 17:18:05 +0500

    feat: add load more button

--- src/modules/leadManagement/leadNotes/index.jsx
+++ src/modules/leadManagement/leadNotes/index.jsx
@@ -4,7 +4,7 @@ import useSWR from 'swr'
 
 import { useMutate } from '@/hooks'
 
-import { Loading } from '@components'
+import { Loading, Button } from '@components'
 
 import { fetchNotes, saveNote } from '@modules/leadManagement/api'
 import { NoteEditForm, NoteActions, LeadNoteSearch, NewNote, LeadInfo } from '@modules/leadManagement/components'
@@ -12,6 +12,8 @@ import { NoteEditForm, NoteActions, LeadNoteSearch, NewNote, LeadInfo } from '@m
 import { decodeJwt, formatDate } from '@utils/helpers'
 import { avatarPlaceholder } from '@constants/profile'
 import { NOTE_INITIAL_STATE } from '@constants/leadManagement'
+
+import { LoadMoreIcon } from '@icons'
 
 const LeadNotes = () => {
     const user = decodeJwt()
@@ -47,34 +49,49 @@ const LeadNotes = () => {
                     <p className='-mt-12 absolute px-3 mx-3 border bg-[#EDFDFB] text-lg tracking-widest border-cyan-200'>
                         Recent<span className='text-sm'> Notes</span>
                     </p>
-                    <div className='px-2 md:px-4'>
+                    <div className='px-2 pb-3 md:px-4'>
                         {data?.length > 0 ? (
-                            data?.map(row => (
-                                <div className='flex gap-x-2.5 pb-2 border-b mb-2' key={row.id}>
-                                    <img
-                                        alt={row?.user?.name || 'Guest'}
-                                        src={row?.user?.avatar ?? avatarPlaceholder}
-                                        onError={e => (e.target.src = avatarPlaceholder)}
-                                        className='h-9 w-9 rounded-full object-cover shadow-sm'
-                                    />
-                                    <div className='flex flex-col w-full gap-y-1'>
-                                        <div className='flex gap-x-6 items-center'>
-                                            <span className='text-sm text-gray-900 capitalize'>
-                                                {row?.user?.name || 'guest'}
-                                            </span>
-                                            <span className='text-xs text-gray-600'>{formatDate(row?.updated_at)}</span>
-                                            {row?.user?.id === user?.user_id && !(note.id && note.id === row?.id) && (
-                                                <NoteActions row={row} note={note} setNote={setNote} mutate={mutate} />
+                            <div className='flex flex-col'>
+                                {data?.map(row => (
+                                    <div className='flex gap-x-2.5 pb-2 border-b mb-2' key={row.id}>
+                                        <img
+                                            alt={row?.user?.name || 'Guest'}
+                                            src={row?.user?.avatar ?? avatarPlaceholder}
+                                            onError={e => (e.target.src = avatarPlaceholder)}
+                                            className='h-9 w-9 rounded-full object-cover shadow-sm'
+                                        />
+                                        <div className='flex flex-col w-full gap-y-1'>
+                                            <div className='flex gap-x-6 items-center'>
+                                                <span className='text-sm text-gray-900 capitalize'>
+                                                    {row?.user?.name || 'guest'}
+                                                </span>
+                                                <span className='text-xs text-gray-600'>
+                                                    {formatDate(row?.updated_at)}
+                                                </span>
+                                                {row?.user?.id === user?.user_id &&
+                                                    !(note.id && note.id === row?.id) && (
+                                                        <NoteActions
+                                                            row={row}
+                                                            note={note}
+                                                            setNote={setNote}
+                                                            mutate={mutate}
+                                                        />
+                                                    )}
+                                            </div>
+                                            {note.id && note.id === row?.id ? (
+                                                <NoteEditForm
+                                                    handleSubmit={handleSubmit}
+                                                    note={note}
+                                                    setNote={setNote}
+                                                />
+                                            ) : (
+                                                <span className='text-black'>{row.message}</span>
                                             )}
                                         </div>
-                                        {note.id && note.id === row?.id ? (
-                                            <NoteEditForm handleSubmit={handleSubmit} note={note} setNote={setNote} />
-                                        ) : (
-                                            <span className='text-black'>{row.message}</span>
-                                        )}
                                     </div>
-                                </div>
-                            ))
+                                ))}
+                                <Button label='Load more' icon={LoadMoreIcon} classes='!w-fit border-0 !mx-auto' />
+                            </div>
                         ) : (
                             <div className='text-sm text-gray-600 pl-14 pt-2 pb-2'>No notes added yet...</div>
                         )}
