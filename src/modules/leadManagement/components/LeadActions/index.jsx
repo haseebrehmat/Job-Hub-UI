29401b3c8ae16fb25ce78db5a1953b377de0df55
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-08-23 20:41:10 +0500

    update: lead actions into dropdown menu

--- src/modules/leadManagement/components/LeadActions/index.jsx
+++ src/modules/leadManagement/components/LeadActions/index.jsx
@@ -1,31 +1,47 @@
-import { memo } from 'react'
+import { memo, useState } from 'react'
 import { Link } from 'react-router-dom'
-
-import { Button, Tooltip } from '@components'
 
 import { can } from '@utils/helpers'
 
-import { SeePassIcon, AssignCandidateIcon } from '@icons'
+import { SeePassIcon, AssignCandidateIcon, ActionsIcons } from '@icons'
 
-const LeadActions = ({ lead, dispatch = null }) => (
-    <div className='flex items-center justify-end gap-1'>
-        {can('view_lead_details') && (
-            <Tooltip text='View lead Details'>
-                <Button
-                    classes='_icon-btn'
-                    icon={SeePassIcon}
-                    onClick={() => dispatch({ draggable: lead?.id, show: true })}
-                />
-            </Tooltip>
-        )}
-        {can('assign_candidate') && (
-            <Tooltip text='Assign Candidate'>
-                <Link to={`/assign-candidate/${lead?.id}`} state={{ candidate: lead?.candidate?.id }}>
-                    {AssignCandidateIcon}
-                </Link>
-            </Tooltip>
-        )}
-    </div>
-)
+const LeadActions = ({ lead, dispatch = null }) => {
+    const [isOpen, setIsOpen] = useState(false)
+    const toggleDropdown = () => setIsOpen(!isOpen)
+    const closeDropdown = () => setIsOpen(false)
 
+    return (
+        <div className='relative inline-block'>
+            <button onClick={toggleDropdown} onBlur={closeDropdown} className={`${isOpen ? 'animate-bounce' : ''}`}>
+                {ActionsIcons}
+            </button>
+            {isOpen && (
+                <div className='absolute right-0 w-max z-40 bg-white border border-gray-300 rounded-md shadow-xl flex flex-col px-1 pt-2.5 pb-2 gap-y-2'>
+                    {can('view_lead_details') && (
+                        <button
+                            onClick={() => {
+                                dispatch({ draggable: lead?.id, show: true })
+                                closeDropdown()
+                            }}
+                            className='bg-transparent border-0 hover:bg-slate-100 hover:text-[#048C8C] !px-2 flex items-center justify-between gap-4'
+                        >
+                            <span>View Lead Details</span>
+                            <span>{SeePassIcon}</span>
+                        </button>
+                    )}
+                    {can('assign_candidate') && (
+                        <Link
+                            to={`/assign-candidate/${lead?.id}`}
+                            state={{ candidate: lead?.candidate?.id }}
+                            className='bg-transparent border-0 hover:bg-slate-100 hover:text-[#048C8C] !px-2 flex items-center justify-between gap-4'
+                        >
+                            <span>Assign Candidate</span>
+                            <span>{AssignCandidateIcon}</span>
+                        </Link>
+                    )}
+                </div>
+            )}
+        </div>
+    )
+}
 export default memo(LeadActions)
