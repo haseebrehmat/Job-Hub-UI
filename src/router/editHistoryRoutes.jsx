a52b326c65caefd3608590233ca7c6e1a6779280
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-08-24 13:36:15 +0500

    fix: title issue in layout

--- src/router/editHistoryRoutes.jsx
+++ src/router/editHistoryRoutes.jsx
@@ -2,7 +2,7 @@ import { EditHistory } from '@modules'
 
 export const editHistoryRoutes = [
     {
-        path: '/edi-history/:rowId',
+        path: '/edit-history/:rowId',
         component: <EditHistory />,
         protect: true,
         title: 'Edit History',
