459d835999e0b33d302a466b577047db173652fa
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-08-28 18:26:03 +0500

    fix: expired job permissions

--- src/utils/constants/permissions.js
+++ src/utils/constants/permissions.js
@@ -239,13 +239,13 @@ export const rawpermissions = [
                 codename: 'view_expired_job',
                 name: 'View Expired Jobs',
                 parent: ['view_job_uploader'],
-                level: 2,
+                level: [2],
             },
             {
                 codename: 'mark_as_expired',
                 name: 'Mark as expired / active',
                 parent: ['view_job_portal', 'view_manual_job', 'view_expired_job'],
-                level: 2,
+                level: [2],
             },
             {
                 codename: 'view_job_portal',
