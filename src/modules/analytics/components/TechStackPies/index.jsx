99524432865e20364e8959b40017aa416cc75448
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-08-29 12:15:01 +0500

    Revert "feat: add `octagon watermark` on charts"

--- src/modules/analytics/components/TechStackPies/index.jsx
+++ src/modules/analytics/components/TechStackPies/index.jsx
@@ -7,16 +7,9 @@ import { formatNum, transformPascal, htmlToPng } from '@utils/helpers'
 import { JOB_TYPES, JOB_TYPE_COLORS2 } from '@constants/analytics'
 
 import { BreadIcon, DownloadIcon2 } from '@icons'
-import logo from '@images/signin-logo.svg'
 
 const TechStackPies = ({ data = {}, stack = null }) => {
     const chartRef = useRef('')
-    const watermark = useRef('')
-    const exportButton = useRef('')
-    const postProcessing = () => {
-        watermark?.current?.classList.add('hidden')
-        exportButton?.current?.classList.remove('hidden')
-    }
 
     const memoizedData = useMemo(
         () =>
@@ -34,22 +27,17 @@ const TechStackPies = ({ data = {}, stack = null }) => {
             .join('')} (${(percent * 100).toFixed(2)}%)`
 
     return (
-        <div className='border px-200000 pt-10 pb-10 text-[#1E6570] mt-10 relative w-1/2' ref={chartRef}>
+        <div className='border px-200000 pt-10 text-[#1E6570] mt-10 relative w-1/2'>
             <p className='-mt-16 absolute px-2 py-1.5 border bg-[#EDFDFB] text-lg tracking-widest'>
                 Tech Stack<span className='text-sm'> - Charts</span>
             </p>
             <span
-                ref={exportButton}
                 className='-mt-14 rounded-full absolute py-1 pr-4 pl-3 border bg-[#EDFDFB] right-2 cursor-pointer text-sm'
-                onClick={() => {
-                    watermark?.current?.classList.remove('hidden')
-                    exportButton?.current?.classList.add('hidden')
-                    htmlToPng(chartRef?.current).then(() => postProcessing())
-                }}
+                onClick={() => htmlToPng(chartRef?.current)}
             >
                 <CustomTooltip text='Export to png'>{DownloadIcon2}Export</CustomTooltip>
             </span>
-            <div id='tech-stack-pies'>
+            <div ref={chartRef} id='tech-stack-pies'>
                 <ResponsiveContainer width='100%' height={400}>
                     <PieChart>
                         <Pie
@@ -88,10 +76,6 @@ const TechStackPies = ({ data = {}, stack = null }) => {
                                 <span className='text-sm'>{transformPascal(JOB_TYPES[key])}</span>
                             </div>
                         ))}
-                </div>
-                <div className='flex items-end justify-end mr-4 py-4 hidden' ref={watermark}>
-                    <span className='text-cyan-900 col-span-3  px-2 font-bold'>Powered by</span>
-                    <img src={logo} alt='' width='120' height='120' />
                 </div>
             </div>
         </div>
--- src/modules/analytics/components/Trends/index.jsx
+++ src/modules/analytics/components/Trends/index.jsx
@@ -18,70 +18,51 @@ import { Tooltip as MyTooltip } from '@components'
 import { htmlToPng } from '@utils/helpers'
 
 import { DownloadIcon2 } from '@icons'
-import logo from '@images/signin-logo.svg'
 
 const StockMarketChart = ({ data = [] }) => {
     const chartRef = useRef('')
-    const watermark = useRef('')
-    const exportButton = useRef('')
-    const postProcessing = () => {
-        watermark?.current?.classList.add('hidden')
-        exportButton?.current?.classList.remove('hidden')
-    }
-
     const lastDataPoint = data[data.length - 1]
     const arrowColor = lastDataPoint.jobs >= data[data.length - 2].jobs ? 'green' : 'red'
 
     return (
-        <div className='border px-2 pt-10 pb-10 mt-10 relative text-[#1E6570]' ref={chartRef}>
+        <div className='border px-2 pt-10 pb-4 mt-10 relative text-[#1E6570]'>
             <p className='-mt-16 absolute px-2 py-1.5 border bg-[#EDFDFB] text-lg tracking-widest'>
                 Jobs Trends<span className='text-sm'> - Market</span>
             </p>
             <span
-                ref={exportButton}
                 className='-mt-14 rounded-full absolute py-1 pr-4 pl-3 border bg-[#EDFDFB] right-2 cursor-pointer text-sm'
-                onClick={() => {
-                    watermark?.current?.classList.remove('hidden')
-                    exportButton?.current?.classList.add('hidden')
-                    htmlToPng(chartRef?.current).then(() => postProcessing())
-                }}
+                onClick={() => htmlToPng(chartRef?.current?.current)}
             >
                 <MyTooltip text='Export to png'>{DownloadIcon2}Export</MyTooltip>
             </span>
-            <div>
-                <ResponsiveContainer width='100%' height={400} id='jobs-trends-chart'>
-                    <LineChart data={data} margin={{ right: 35, top: 20 }}>
-                        <CartesianGrid strokeDasharray='12 12' />
-                        <XAxis dataKey='date' />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <Line
-                            type='linear'
-                            dataKey='jobs'
-                            stroke='#4ab9a7'
-                            activeDot={{ r: 10 }}
-                            dot={{ r: 7 }}
-                            strokeWidth={4}
-                            strokeOpacity={0.7}
-                        >
-                            <LabelList dataKey='jobs' position='insideTopLeft' fill='black' />
-                        </Line>
-                        <ReferenceLine y={lastDataPoint.jobs} stroke={arrowColor} strokeDasharray='3 3'>
-                            <Label value={lastDataPoint.jobs} position='top' fill={arrowColor} />
-                            <Label
-                                value={arrowColor === 'green' ? '\u25B2' : '\u25BC'}
-                                position='bottom'
-                                fill={arrowColor}
-                            />
-                        </ReferenceLine>
-                    </LineChart>
-                </ResponsiveContainer>
-                <div className='flex items-end justify-end mr-4 py-4 hidden' ref={watermark}>
-                    <span className='text-cyan-900 col-span-3  px-2 font-bold'>Powered by</span>
-                    <img src={logo} alt='' width='120' height='120' />
-                </div>
-            </div>
+            <ResponsiveContainer width='100%' height={400} ref={chartRef} id='jobs-trends-chart'>
+                <LineChart data={data} margin={{ right: 35, top: 20 }}>
+                    <CartesianGrid strokeDasharray='12 12' />
+                    <XAxis dataKey='date' />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line
+                        type='linear'
+                        dataKey='jobs'
+                        stroke='#4ab9a7'
+                        activeDot={{ r: 10 }}
+                        dot={{ r: 7 }}
+                        strokeWidth={4}
+                        strokeOpacity={0.7}
+                    >
+                        <LabelList dataKey='jobs' position='insideTopLeft' fill='black' />
+                    </Line>
+                    <ReferenceLine y={lastDataPoint.jobs} stroke={arrowColor} strokeDasharray='3 3'>
+                        <Label value={lastDataPoint.jobs} position='top' fill={arrowColor} />
+                        <Label
+                            value={arrowColor === 'green' ? '\u25B2' : '\u25BC'}
+                            position='bottom'
+                            fill={arrowColor}
+                        />
+                    </ReferenceLine>
+                </LineChart>
+            </ResponsiveContainer>
         </div>
     )
 }
