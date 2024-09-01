7b5e98f89b5e4b38ed581e76975130735d94b140
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-09-01 19:51:51 +0500

    fix: remove delete functionality from `TeamForm`

--- src/modules/userManagement/components/TeamForm/index.jsx
+++ src/modules/userManagement/components/TeamForm/index.jsx
@@ -2,17 +2,15 @@ import { memo, useState } from 'react'
 import { toast } from 'react-hot-toast'
 import useSWR from 'swr'
 
-import { useMutate, useDelete } from '@/hooks'
-import { Button, Drawer, Input, CustomDilog } from '@components'
-import CustomSelector from '@components/CustomSelector'
-import { RolesDropdown } from '@modules/userManagement/components'
+import { useMutate } from '@/hooks'
+
+import { Button, Drawer, Input, CustomSelector } from '@components'
+
+import { RolesDropdown, RoleUsersDropdown } from '@modules/userManagement/components'
 import { saveTeam, fetchRoleWiseUsers, fetchDropdownUsers } from '@modules/userManagement/api'
 
 import { teamSchema } from '@utils/schemas'
 import { getMsg, parseMembers } from '@utils/helpers'
-import RoleUsersDropdown from '../ReportingToDropdown'
-import { TrashIcon } from '@icons'
-import { can } from '@/utils/helpers'
 
 const TeamForm = ({ show, setShow, mutate, team }) => {
     const [role, setRole] = useState(team?.reporting_to?.roles?.id)
@@ -38,22 +36,6 @@ const TeamForm = ({ show, setShow, mutate, team }) => {
     )
     const { data, isLoading: uLoading } = useSWR(`/api/auth/role_users/${role}/`, role ? fetchRoleWiseUsers : null)
     const { data: fetched, isLoading } = useSWR('/api/auth/user/?type=dropdown', fetchDropdownUsers)
-
-    const { wait, confirm } = useDelete(`/api/auth/team/${team?.id}/`)
-
-    const deleteRole = async () => {
-        const result = await confirm()
-        if (result && wait === false) {
-            setShow(false)
-            mutate('/api/auth/team/')
-        }
-    }
-
-    const { CustomModal, openModal } = CustomDilog(
-        'Confirm delete?',
-        `Are you sure want to delete the team: ${team?.name}`,
-        deleteRole
-    )
 
     return (
         <Drawer show={show} setShow={setShow} w='320px'>
@@ -91,16 +73,6 @@ const TeamForm = ({ show, setShow, mutate, team }) => {
                     <div className='pt-4 space-y-2'>
                         <Button label={team?.id ? 'Update' : 'Submit'} type='submit' fill />
                         <Button label='Cancel' onClick={() => setShow(false)} />
-                        {/* {CustomModal}
-
-                        {can('delete_team') && team?.id && (
-                            <Button
-                                label='Delete'
-                                classes='bg-transparent text-red-500 border-red-500'
-                                icon={TrashIcon}
-                                onClick={openModal}
-                            />
-                        )} */}
                     </div>
                 </div>
             </form>
