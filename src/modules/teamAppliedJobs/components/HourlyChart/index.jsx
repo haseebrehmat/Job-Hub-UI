714c321d616d1d7cde5bf4786bed32e529ea5618
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-09-05 14:45:44 +0500

    feat: update `HourlyChart` and integrate api

--- src/modules/teamAppliedJobs/components/HourlyChart/index.jsx
+++ src/modules/teamAppliedJobs/components/HourlyChart/index.jsx
@@ -1,69 +1,28 @@
 import { memo, useState } from 'react'
+import useSWR from 'swr'
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts'
 
 import { CustomSelector } from '@components'
 
-import { parseMembers } from '@utils/helpers'
+import { fetchTeamAppliedJobsPerHour } from '@modules/teamAppliedJobs/api'
+
+import { formatDate2, parseMembers } from '@utils/helpers'
+
+import { UptoIcon } from '@icons'
 
 const HourlyChart = ({ members = null }) => {
     const [selectedMember, setSelectedMember] = useState({ value: 'all', label: 'All Team Members' })
 
-    const data = [
-        {
-            time: '12:00 PM',
-            jobs: 23,
-        },
-        {
-            time: '01:00 AM',
-            jobs: 345,
-        },
-        {
-            time: '02:00 AM',
-            jobs: 0,
-        },
-        {
-            time: '03:00 AM',
-            jobs: 23,
-        },
-        {
-            time: '04:00 AM',
-            jobs: 12,
-        },
-        {
-            time: '05:00 AM',
-            jobs: 120,
-        },
-        {
-            time: '06:00 AM',
-            jobs: 60,
-        },
-        {
-            time: '07:00 AM',
-            jobs: 1,
-        },
-        {
-            time: '08:00 AM',
-            jobs: 90,
-        },
-        {
-            time: '09:00 AM',
-            jobs: 20,
-        },
-        {
-            time: '10:00 AM',
-            jobs: 7,
-        },
-        {
-            time: '11:00 AM',
-            jobs: 79,
-        },
-        {
-            time: '12:00 AM',
-            jobs: 65,
-        },
-    ]
+    const { data, isLoading } = useSWR(
+        `api/job_portal/team_applied_jobs_memberwise_analytics/?member=${
+            selectedMember?.value === 'all' ? '' : selectedMember?.value
+        }`,
+        fetchTeamAppliedJobsPerHour
+    )
 
-    return (
+    return isLoading ? (
+        <span className='text-4xl flex justify-center pt-10 text-[#048C8C] animate-pulse'>Loading ...</span>
+    ) : (
         <div className='flex flex-col'>
             <div className='flex px-4 pb-2 items-end justify-between'>
                 <span className='text-[#048C8C] font-semibold italic tracking-wider'>
@@ -78,24 +37,19 @@ const HourlyChart = ({ members = null }) => {
                     />
                 </span>
             </div>
-            <ResponsiveContainer
-                width='98%'
-                height={400}
-                minWidth={800}
-                className='border rounded-lg _shadow-2 bg-[#EDFFFB] mx-auto'
-            >
-                <LineChart data={data} margin={{ top: 40, bottom: 50, right: 30, left: 20 }}>
+            <ResponsiveContainer width='98%' height={400} className='border rounded-lg _shadow-2 bg-[#EDFFFB] mx-auto'>
+                <LineChart data={data?.results} margin={{ top: 40, bottom: 50, right: 30, left: 20 }}>
                     <CartesianGrid strokeDasharray='3 3' />
                     <XAxis
                         dataKey='time'
                         stroke='#037571'
-                        angle={-25}
+                        angle={-27}
                         interval={0}
                         textAnchor='end'
                         allowDuplicatedCategory={false}
-                        fontSize={14}
+                        fontSize={15}
                     />
-                    <YAxis stroke='#037571' type='number' domain={[0, 'auto']} />
+                    <YAxis stroke='#037571' type='number' fontSize={14} />
                     <Tooltip />
                     <Line
                         dataKey='jobs'
@@ -105,6 +59,13 @@ const HourlyChart = ({ members = null }) => {
                     />
                 </LineChart>
             </ResponsiveContainer>
+            {data?.dates && (
+                <div className='text-sm flex px-4 pt-2 items-center justify-end text-[#048C8C] tracking-widest'>
+                    {formatDate2(data?.dates?.start_date)}
+                    &nbsp;{UptoIcon}&nbsp;
+                    {formatDate2(data?.dates?.end_date)}
+                </div>
+            )}
         </div>
     )
 }
