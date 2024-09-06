585cc4aa3c96fa770b63c3ec04e558a3e759804c
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-09-06 17:37:05 +0500

    update: tweak `GroupScraperLinks` with links summary

--- src/modules/scrapper/components/GroupLinks/index.jsx
+++ src/modules/scrapper/components/GroupLinks/index.jsx
@@ -1,15 +1,14 @@
 import { memo, useState } from 'react'
 import useSWR from 'swr'
 
-import { Button, EmptyTable, Loading, Badge, Tooltip } from '@components'
+import { Button, Loading } from '@components'
 
 import { GroupLinkActions, GroupLinksForm } from '@modules/scrapper/components'
 import { fetchGroupLinks } from '@modules/scrapper/api'
 
 import { can, formatStringInPascal } from '@utils/helpers'
-import { GROUP_SOURCE_LINK_HEADS } from '@constants/scrapper'
 
-import { CreateIcon } from '@icons'
+import { CreateIcon, UptoIcon } from '@icons'
 
 const GroupLinks = () => {
     const [link, setLink] = useState()
@@ -26,56 +25,55 @@ const GroupLinks = () => {
 
     return (
         <div className='max-w-full overflow-x-auto mb-14'>
-            <div className='flex items-center space-x-4 pb-6'>
+            <div className='flex items-center space-x-4 pb-4'>
                 {can('create_job_source_link') && (
                     <Button label='Create Group Links' fit icon={CreateIcon} onClick={() => handleClick()} />
                 )}
             </div>
-            <table className='table-auto w-full text-sm text-left text-[#048C8C]'>
-                <thead className='text-xs uppercase border border-[#048C8C]'>
-                    <tr>
-                        {GROUP_SOURCE_LINK_HEADS.map(heading => (
-                            <th scope='col' className='px-3 py-4' key={heading}>
-                                {heading}
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody className='bg-white'>
-                    {data?.grouplinks?.length > 0 && !error ? (
-                        data?.grouplinks?.map((row, idx) => (
-                            <tr className='border-b border-[#006366] border-opacity-30 hover:bg-gray-100' key={row.id}>
-                                <td className='px-3 py-6'>{idx + 1}</td>
-                                <td className='px-3 py-6'>{formatStringInPascal(row?.group_scraper?.name)}</td>
-                                <td className='px-3 py-6'>
-                                    {row?.queries?.length > 0 &&
-                                        row?.queries.map((q, index) => (
-                                            <span className='font-mono m-1 inline-block' key={index}>
-                                                <a href={q.link} target='_blank' rel='noreferrer'>
-                                                    <Tooltip text={q.link}>
-                                                        <Badge
-                                                            label={`Link ${index + 1} | ${q.job_type} | ${
-                                                                q.job_source
-                                                            } `}
-                                                            type='success'
-                                                        />
-                                                    </Tooltip>
-                                                </a>
-                                            </span>
-                                        ))}
-                                </td>
-                                <td className='px-3 py-6 float-right'>
-                                    {can(['edit_job_source_link', 'delete_job_source_link']) && (
-                                        <GroupLinkActions id={row?.id} edit={() => handleClick(row)} mutate={mutate} />
-                                    )}
-                                </td>
-                            </tr>
-                        ))
-                    ) : (
-                        <EmptyTable cols={6} msg='No group links / urls found yet!' />
-                    )}
-                </tbody>
-            </table>
+            <div className='grid grid-cols-1 gap-2 md:grid-cols-2'>
+                {data?.grouplinks?.length > 0 && !error ? (
+                    data?.grouplinks.map((row, idx) => (
+                        <div className='bg-white rounded-md p-4 border relative text-[#338d8c]' key={idx}>
+                            <h2 className='text-2xl'>{formatStringInPascal(row?.group_scraper?.name)}</h2>
+                            {can(['edit_job_source_link', 'delete_job_source_link']) && (
+                                <GroupLinkActions id={row?.id} edit={() => handleClick(row)} mutate={mutate} />
+                            )}
+                            <div className='flex flex-col mt-2 ml-2 text-sm'>
+                                <div className='mb-2 flex justify-between'>
+                                    <span className='tracking-wider italic'>Links Summary</span>
+                                    <span className='underline underline-offset-4 inline-flex gap-2 items-center text-xs'>
+                                        View Details {UptoIcon}
+                                    </span>
+                                </div>
+                                <div className='flex flex-wrap gap-x-3 gap-y-3.5 mt-3'>
+                                    <div className='flex items-center gap-6 px-2.5 py-1 rounded-full cursor-pointer border border-blue-400 text-blue-400'>
+                                        <span>Total</span>
+                                        <span className='font-mono'>10</span>
+                                    </div>
+                                    <div className='flex items-center gap-6 px-2.5 py-1 rounded-full cursor-pointer border border-orange-400 text-orange-400'>
+                                        <span>Stopped</span>
+                                        <span className='font-mono'>10</span>
+                                    </div>
+                                    <div className='flex items-center gap-6 px-2.5 py-1 rounded-full cursor-pointer border border-[#338d8c] text-[#338d8c]'>
+                                        <span>Running</span>
+                                        <span className='font-mono'>1</span>
+                                    </div>
+                                    <div className='flex items-center gap-6 px-2.5 py-1 rounded-full cursor-pointer border border-red-400 text-red-400'>
+                                        <span>Failed</span>
+                                        <span className='font-mono'>10</span>
+                                    </div>
+                                    <div className='flex items-center gap-6 px-2.5 py-1 rounded-full cursor-pointer border border-purple-400 text-purple-400'>
+                                        <span>Completed</span>
+                                        <span className='font-mono'>23</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <span className='ml-2 text-gray-500'>No group links / urls found yet!</span>
+                )}
+            </div>
             {can(['edit_job_source_link', 'delete_job_source_link']) && show && (
                 <GroupLinksForm show={show} setShow={setShow} mutate={mutate} link={link} />
             )}
