e3648805e17cf43211306535d5302db08d2c9b08
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-09-06 20:18:06 +0500

    feat: add running link box

--- src/modules/scrapper/components/GroupLinks/index.jsx
+++ src/modules/scrapper/components/GroupLinks/index.jsx
@@ -67,6 +67,20 @@ const GroupLinks = () => {
                                         <span className='font-mono'>23</span>
                                     </div>
                                 </div>
+                                <div className='mt-4 flex justify-between items-center p-3 bg-slate-100 border border-[#338d8c] border-opacity-50 rounded-lg'>
+                                    <span className='font-semibold'>Running Link:</span>
+                                    <span>Monster</span>
+                                    <span>Contract On Site</span>
+                                    <a
+                                        href='google.com'
+                                        target='_blank'
+                                        rel='noreferrer'
+                                        className='flex items-center gap-1.5 border border-[#4ab9a7] rounded-full px-2'
+                                    >
+                                        <span className='animate-ping'>{UptoIcon}</span>
+                                        <span>Visit link</span>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     ))
