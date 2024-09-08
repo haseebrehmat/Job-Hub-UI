abb5fc69acad99a390e06cccf507c43cee342b65
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-09-08 16:01:42 +0500

    feat: add `PortalLayout` component

--- src/modules/jobPortal-v2/components/index.jsx
+++ src/modules/jobPortal-v2/components/index.jsx
@@ -1,3 +1,4 @@
+import PortalLayout from '@modules/jobPortal-v2/components/PortalLayout'
 import JobCard from '@modules/jobPortal-v2/components/JobCard'
 
-export { JobCard }
+export { PortalLayout, JobCard }
