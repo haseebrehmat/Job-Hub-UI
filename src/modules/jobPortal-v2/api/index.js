2ef24f639091abce37f33d083ca96aea113a9d45
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-09-11 19:34:41 +0500

    feat: add Download Csv

--- src/modules/jobPortal-v2/api/index.js
+++ src/modules/jobPortal-v2/api/index.js
@@ -1,17 +1,22 @@
+import toast from 'react-hot-toast'
+
 import { http } from '@utils/http'
 
 import { isset } from '@utils/helpers'
 
+const getFilterAppliedURL = filters =>
+    `&tech_keywords=${filters?.techs?.map(tech => tech.value)}&job_source=${filters?.sources?.map(
+        source => source.value
+    )}&ordering=${filters?.order ?? '-job_posted_date'}&job_visibility=${filters?.visible ?? 'all'}&from_date=${
+        filters?.from
+    }&to_date=${filters?.to}&job_type=${filters?.types?.map(type => type.value)}&blocked=${filters?.blocked}`
+
 export const fetchJobs = (page = 1, query = '', filters = {}) => {
     let url = `api/job_portal/job_details/?page=${page}&search=${query}`
 
-    if (isset(filters)) {
-        url += `&tech_keywords=${filters?.techs?.map(tech => tech.value)}&job_source=${filters?.sources?.map(
-            source => source.value
-        )}&ordering=${filters?.order ?? '-job_posted_date'}&job_visibility=${filters?.visible ?? 'all'}&from_date=${
-            filters?.from
-        }&to_date=${filters?.to}&job_type=${filters?.types?.map(type => type.value)}&blocked=${filters?.blocked}`
-    }
+    console.log(filters)
+
+    if (isset(filters)) url += getFilterAppliedURL(filters)
 
     return http.get(url).then(({ data }) => ({ jobs: data?.data }))
 }
@@ -19,17 +24,21 @@ export const fetchJobs = (page = 1, query = '', filters = {}) => {
 export const fetchJobFilters = (query = '', filters = {}) => {
     let url = `api/job_portal/job_details/?page=1&search=${query}`
 
-    if (isset(filters)) {
-        url += `&tech_keywords=${filters?.techs?.map(tech => tech.value)}&job_source=${filters?.sources?.map(
-            source => source.value
-        )}&ordering=${filters?.order ?? '-job_posted_date'}&job_visibility=${filters?.visible ?? 'all'}&from_date=${
-            filters?.from
-        }&to_date=${filters?.to}&job_type=${filters?.types?.map(type => type.value)}&blocked=${filters?.blocked}`
-    }
+    if (isset(filters)) url += getFilterAppliedURL(filters)
 
     return http.get(url).then(({ data }) => ({
         techStacks: data?.tech_keywords_count_list,
         jobSources: data?.job_source_count_list,
         jobTypes: data?.total_job_type,
     }))
+}
+
+export const downloadJobsData = async (query = '', filters = {}) => {
+    let url = `api/job_portal/job_details/?page=1&search=${query}`
+
+    if (isset(filters)) url += getFilterAppliedURL(filters)
+    url += '&download=true'
+
+    const { data } = await http.get(url)
+    return toast.success(data || 'Export in progress, You will be notify through email', { icon: '🎉', duration: 2000 })
 }
--- src/modules/jobPortal-v2/sections/Taskbar/index.jsx
+++ src/modules/jobPortal-v2/sections/Taskbar/index.jsx
@@ -1,26 +1,36 @@
 import { memo } from 'react'
 import { Tooltip } from 'react-tooltip'
+import useSWRMutation from 'swr/mutation'
 
 import { Input, Button } from '@components'
 
 import { useJobPortalV2Store } from '@/stores'
 
+import { downloadJobsData } from '@modules/jobPortal-v2/api'
 import { OrderBy, Visibility } from '@modules/jobPortal-v2/components'
 
-import { SearchIcon, GridViewIcon, ListViewIcon } from '@icons'
+import { SearchIcon, GridViewIcon, ListViewIcon, DownloadIcon } from '@icons'
 
 const Taskbar = () => {
-    const [query, view, setQuery, applySearch, toggleView] = useJobPortalV2Store(state => [
+    const [query, paramQuery, view, filters, setQuery, applySearch, toggleView] = useJobPortalV2Store(state => [
         state?.query,
+        state?.paramQuery,
         state?.view,
+        state?.params,
         state?.setQuery,
         state?.setParams,
         state?.toggleView,
     ])
 
+    const { trigger, isMutating } = useSWRMutation([paramQuery, filters], () => downloadJobsData(paramQuery, filters), {
+        revalidateOnReconnect: false,
+        shouldRetryOnError: false,
+        revalidateOnFocus: false,
+    })
+
     return (
         <div className='flex items-center justify-between gap-2 bg-slate-100 border border-slate-300 rounded-xl p-2.5 text-[#048C8C] text-sm'>
-            <div className='flex items-center w-1/2'>
+            <div className='flex items-center gap-3 w-1/2'>
                 <div className='relative hidden md:block flex-1 bg-white'>
                     <Input
                         ph='Search by typing keywords...'
@@ -34,6 +44,14 @@ const Taskbar = () => {
             <div className='flex gap-2 w-fit'>
                 <OrderBy />
                 <Visibility />
+                <Button
+                    icon={DownloadIcon}
+                    fit
+                    disabled={isMutating}
+                    classes='download-csv !py-[5px] !px-1.5 !m-0 !flex !items-center'
+                    onClick={() => trigger()}
+                />
+                <Tooltip anchorSelect='.download-csv' content={isMutating ? 'Downloading...' : 'Download Csv'} />
                 <Button
                     icon={view === 'list' ? ListViewIcon : GridViewIcon}
                     fit
