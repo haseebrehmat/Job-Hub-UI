889bb1017283a9333f58ee377deb6593de5a635b
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-09-13 16:06:13 +0500

    update: show blocked jobs

--- src/modules/jobPortal-v2/components/JobCardFooter/index.jsx
+++ src/modules/jobPortal-v2/components/JobCardFooter/index.jsx
@@ -1,16 +1,20 @@
 import { memo } from 'react'
 
+import { useJobPortalV2Store } from '@/stores'
+
 import { Button } from '@components'
 
 import { timeSince } from '@utils/helpers'
 
 import { UptoIcon } from '@icons'
 
-const JobCardFooter = ({ job = null }) =>
-    job ? (
+const JobCardFooter = ({ job = null }) => {
+    const [blocked] = useJobPortalV2Store(state => [state.blocked])
+
+    return job ? (
         <div className='flex items-center justify-between mt-2 mr-1.5'>
             <span className='text-slate-500 font-semibold -skew-x-3 pl-2'>{timeSince(job?.job_posted_date)}</span>
-            {job?.total_vertical > 0 ? (
+            {job?.total_vertical > 0 && !blocked ? (
                 <div className='flex items-center gap-2'>
                     <span className='text-sm border rounded px-2 p-0.5 text-[#338d8c] border-[#55bf84] font-mono'>
                         {job?.total_vertical} / <strong>{job?.remaining_vertical}</strong>
@@ -26,10 +30,11 @@ const JobCardFooter = ({ job = null }) =>
                 </div>
             ) : (
                 <small className='text-xs text-slate-500 border py-0.5 px-1.5 border-slate-500 rounded-full'>
-                    No vertical assigned
+                    {blocked ? 'blocked' : 'No vertical assigned'}
                 </small>
             )}
         </div>
     ) : null
+}
 
 export default memo(JobCardFooter)
