667b7edd344b30866b1b35233eba8256dd7a1f99
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-09-14 17:33:12 +0500

    feat: add badges in `TechKeywords`

--- src/modules/jobPortal-v2/components/TechKeywords/index.jsx
+++ src/modules/jobPortal-v2/components/TechKeywords/index.jsx
@@ -1,11 +1,15 @@
 import { memo } from 'react'
 
+import { Badge } from '@components'
+
 const TechKeywords = ({ keywords }) =>
     keywords ? (
         <span className='flex items-center gap-2 border border-neutral-400 px-2 py-1 rounded-md text-[14px]'>
             <span className='text-neutral-500'>Tech Stacks:</span>
-            <span className='text-neutral-700 capitalize tracking-wider'>
-                {Array.from({ length: 5 }, () => ` ${keywords},`)}
+            <span className='text-neutral-700 capitalize tracking-wider inline-flex gap-2 text-xs'>
+                {Array.from({ length: 5 }, () => (
+                    <Badge label={keywords} type='success' classes='!text-xs' />
+                ))}
             </span>
         </span>
     ) : null
