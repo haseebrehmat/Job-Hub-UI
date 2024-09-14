5bed9d9b14e2eb4ae00e27c57bf111514f1b7b90
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-09-14 14:57:16 +0500

    feat: add view job details Link

--- src/modules/jobPortal-v2/components/JobCardHead/index.jsx
+++ src/modules/jobPortal-v2/components/JobCardHead/index.jsx
@@ -1,4 +1,5 @@
 import { memo } from 'react'
+import { Link } from 'react-router-dom'
 
 import { useVisitedJobsStore } from '@/stores'
 
@@ -15,11 +16,20 @@ const JobCardHead = ({ job = null }) => {
         job && (
             <div className='flex flex-col lg:flex-row items-start justify-between'>
                 <div className='flex gap-3 items-start'>
-                    <span className='bg-[#338d8c] h-14 w-14 rounded-lg text-white uppercase flex items-center justify-center text-xl'>
+                    <span className='bg-[#338d8c] h-12 w-12 rounded-lg text-white uppercase flex items-center justify-center text-lg'>
                         {job?.job_source?.substring(0, 1)}
                     </span>
                     <div className='flex flex-col text-gray-600 gap-y-0.5'>
-                        <span className='text-lg capitalize font-semibold flex-wrap'>{job?.job_title}</span>
+                        <Link
+                            className='text-lg capitalize font-semibold flex-wrap'
+                            to={`/job-details/${job?.id}`}
+                            state={{
+                                data: job,
+                                title: 'Job Details',
+                            }}
+                        >
+                            {job?.job_title}
+                        </Link>
                         <span className='text-sm capitalize italic inline-flex items-center gap-1'>
                             {isset(job?.address) ? (
                                 <>
