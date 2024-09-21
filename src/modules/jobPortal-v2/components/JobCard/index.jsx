2d68ef311c07d40cccecb3ff39b4b8e69d1ccec5
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-09-21 17:23:41 +0500

    update: make JobPortalv2`s components responsive

--- src/modules/jobPortal-v2/components/JobCard/index.jsx
+++ src/modules/jobPortal-v2/components/JobCard/index.jsx
@@ -11,7 +11,7 @@ const JobCard = ({ job = null }) =>
         >
             <JobCardHead job={job} />
             <div className='pl-1.5'>
-                <div className='grid grid-cols-[minmax(0,1fr),minmax(0,1fr)] gap-y-3 gap-x-4 pt-3.5'>
+                <div className='grid md:grid-cols-[minmax(0,1fr),minmax(0,1fr)] gap-y-3 gap-x-4 pt-3.5'>
                     {job?.company_name && (
                         <span className='flex items-center gap-2 border border-neutral-400 px-2 py-1 rounded-md text-[14px] h-fit'>
                             <span className='text-neutral-500'>Company:</span>
--- src/modules/jobPortal-v2/components/JobCardFooter/index.jsx
+++ src/modules/jobPortal-v2/components/JobCardFooter/index.jsx
@@ -13,7 +13,7 @@ const JobCardFooter = ({ job = null }) => {
 
     return job ? (
         <div className='flex items-center justify-between mt-3 mr-1.5'>
-            <span className='text-slate-500 font-semibold -skew-x-3 pl-2 inline-flex items-center gap-1'>
+            <span className='text-slate-500 font-semibold -skew-x-3 pl-2 inline-flex items-center gap-1 flex-wrap'>
                 <span className='text-sm'>Posted</span>
                 {timeSince(job?.job_posted_date)}
                 <span className='text-sm font-normal'>({formatDate(job?.job_posted_date)})</span>
