026a26a1db30b547c18d17dcf89b7502732935eb
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-09-21 19:35:58 +0500

    update: tweak filter and reset buttons

--- src/modules/jobPortal-v2/components/ExplicitApply/index.jsx
+++ src/modules/jobPortal-v2/components/ExplicitApply/index.jsx
@@ -6,11 +6,11 @@ const ExplicitApply = () => {
     const [apply, reset] = useJobPortalV2Store(state => [state?.applyFilters, state?.resetFilters])
 
     return (
-        <div className='w-full flex items-center gap-2 justify-center text-sm md:text-[15px] -my-2'>
-            <span className='cursor-pointer border px-2 py-1 rounded-lg border-[#338d8c]' onClick={() => apply()}>
-                Search
+        <div className='w-full flex items-center gap-2 justify-center text-sm md:text-[15px]'>
+            <span className='cursor-pointer border px-4 py-1 rounded-lg border-[#338d8c]' onClick={() => apply()}>
+                Filter
             </span>
-            <span className='cursor-pointer border px-2 py-1 rounded-lg border-[#338d8c]' onClick={() => reset()}>
+            <span className='cursor-pointer border px-4 py-1 rounded-lg border-[#338d8c]' onClick={() => reset()}>
                 Reset
             </span>
         </div>
