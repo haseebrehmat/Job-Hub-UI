7405de6d33ab5ca8548bc014af58b397c7f09487
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-09-21 16:05:55 +0500

    feat: fetch job sources in instant scraper from API

--- src/modules/scrapper/components/SyncNow/index.jsx
+++ src/modules/scrapper/components/SyncNow/index.jsx
@@ -1,19 +1,25 @@
 import { memo, useState, useRef, useEffect } from 'react'
 import useSWRMutation from 'swr/mutation'
 import useSWRImmutable from 'swr/immutable'
+import useSWR from 'swr'
 
 import { Button, Input } from '@components'
 
 import { syncNow, getScrapperCycleStatus, toggleScrapperCycleStatus } from '@modules/scrapper/api'
-
-import { JOB_SOURCES_INITIAL_VALS } from '@constants/scrapper'
+import { fetchJobSources } from '@modules/settings/api'
 
 import { RunScrapperIcon, PauseIcon } from '@icons'
 
 const SyncNow = () => {
     const [isOpen, setIsOpen] = useState(false)
-    const [sources, setSources] = useState(JOB_SOURCES_INITIAL_VALS)
+    const [query, setQuery] = useState('')
     const divRef = useRef(null)
+
+    const {
+        data: sourcesData,
+        error,
+        isLoading: sourcesLoading,
+    } = useSWR('api/job_scraper/job_source/', fetchJobSources)
 
     const { data, isLoading, mutate } = useSWRImmutable('api/job_scraper/sync_scheduler/', getScrapperCycleStatus)
 
@@ -44,10 +50,7 @@ const SyncNow = () => {
     }
 
     const handleClickOutside = event => {
-        if (divRef.current && !divRef.current.contains(event.target)) {
-            setIsOpen(false)
-            setSources(JOB_SOURCES_INITIAL_VALS)
-        }
+        if (divRef.current && !divRef.current.contains(event.target)) setIsOpen(false)
     }
 
     useEffect(() => {
@@ -57,32 +60,25 @@ const SyncNow = () => {
         }
     })
 
-    const handleTriggerforScrapper = () => trigger2()
-    const filterSources = e =>
-        setSources(
-            JOB_SOURCES_INITIAL_VALS.filter(job => job?.label.toLowerCase().includes(e.target?.value.toLowerCase()))
-        )
-
     return (
         <div className='relative inline-block text-left' ref={divRef}>
             <div className='flex'>
-                <Button
-                    label={isMutating1 ? 'Running........' : 'Run Scrapper Now'}
-                    fit
-                    icon={RunScrapperIcon}
-                    onClick={() => {
-                        setIsOpen(!isOpen)
-                        setSources(JOB_SOURCES_INITIAL_VALS)
-                    }}
-                    disabled={isMutating1}
-                    classes='mx-2'
-                />
+                {!sourcesLoading && (
+                    <Button
+                        label={isMutating1 ? 'Running........' : 'Run Scrapper Now'}
+                        fit
+                        icon={RunScrapperIcon}
+                        onClick={() => setIsOpen(!isOpen)}
+                        disabled={isMutating1}
+                        classes='mx-2'
+                    />
+                )}
                 {!isMutating2 && !isLoading && (
                     <Button
                         label={data ? 'Stop ' : 'Start '}
                         fit
                         icon={data ? PauseIcon : RunScrapperIcon}
-                        onClick={() => handleTriggerforScrapper()}
+                        onClick={() => trigger2()}
                         disabled={isLoading}
                         fill={!data}
                         classes='disabled:opacity-25'
@@ -95,17 +91,30 @@ const SyncNow = () => {
                     tabIndex={-1}
                 >
                     <div className='p-1 flex flex-col'>
-                        <Input ph='searh by typing..' classes='!h-8' onChange={filterSources} />
-                        {sources.map(({ label, value }) => (
-                            <button
-                                className='text-start px-2 py-2 text-sm hover:bg-gray-100'
-                                onClick={() => runSingleScraper(value)}
-                                disabled={isMutating1}
-                                key={value}
-                            >
-                                {label}
-                            </button>
-                        ))}
+                        <Input
+                            ph='Search by typing..'
+                            classes='!h-8'
+                            onChange={e => setQuery(e.target.value)}
+                            value={query}
+                        />
+                        {sourcesData?.sources?.length > 0 && !error ? (
+                            sourcesData?.sources
+                                ?.filter(row =>
+                                    query !== '' ? row?.name?.toLowerCase().includes(query?.toLowerCase()) : row
+                                )
+                                ?.map(({ name, key }) => (
+                                    <button
+                                        className='text-start px-2 py-2 text-sm hover:bg-gray-100'
+                                        onClick={() => runSingleScraper(key)}
+                                        disabled={isMutating1}
+                                        key={key}
+                                    >
+                                        {name}
+                                    </button>
+                                ))
+                        ) : (
+                            <span className='text-start px-2 py-2 text-sm'>No job sources found yet!</span>
+                        )}
                     </div>
                 </div>
             )}
