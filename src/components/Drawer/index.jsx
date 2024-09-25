8c82b5ff54e8f7fc678b32495809cc6ff4e5018f
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-09-25 18:33:28 +0500

    feat: add header prop to `Drawer` component

--- src/components/Drawer/index.jsx
+++ src/components/Drawer/index.jsx
@@ -1,10 +1,18 @@
 import { memo } from 'react'
-
 import SlidingPane from 'react-sliding-pane'
 import 'react-sliding-pane/dist/react-sliding-pane.css'
 
-const Drawer = ({ w = '250px', show, setShow, classes = null, dir = 'right', children }) => (
-    <SlidingPane isOpen={show} from={dir} width={w} onRequestClose={() => setShow(!show)} hideHeader>
+import { isset } from '@utils/helpers'
+
+const Drawer = ({ w = '250px', show, setShow, classes = null, dir = 'right', header = null, children }) => (
+    <SlidingPane
+        isOpen={show}
+        from={dir}
+        width={w}
+        onRequestClose={() => setShow(!show)}
+        title={header}
+        hideHeader={!isset(header)}
+    >
         <div className={`text-[#048C8C] p-3 h-fit ${classes}`}>{children}</div>
     </SlidingPane>
 )
