2b10a3d8034027a5581fd7070bccc9558937b6cf
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-09-26 18:55:52 +0500

    fix: make lead table reponsive

--- src/modules/leadManagement/leads/table.jsx
+++ src/modules/leadManagement/leads/table.jsx
@@ -32,57 +32,59 @@ const LeadsTable = () => {
             {vals.draggable && <LeadModal vals={vals} dispatch={dispatch} refetch={mutate} />}
             <div className='flex flex-col gap-3 px-4'>
                 <LeadSearchAndFilters filtered={vals} dispatch={dispatch} />
-                <table className='table-auto w-full overflow-y-auto max-h-[500px] text-sm text-left text-[#048C8C]'>
-                    <thead className='text-xs uppercase border border-[#048C8C]'>
-                        <tr>
-                            {LEAD_HEADS.map(heading => (
-                                <th scope='col' className='px-3 py-4' key={heading}>
-                                    {heading}
-                                </th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data?.leads?.length > 0 && !error ? (
-                            data?.leads?.map((row, idx) => (
-                                <tr
-                                    className='bg-white border-b border-[#006366] border-opacity-30 hover:bg-slate-100'
-                                    key={row.id}
-                                >
-                                    <td className='px-3 py-4 w-10'>{idx + 1}</td>
-                                    <td className='px-3 py-4 sm:w-1/5'>{row?.applied_job?.title || '-'}</td>
-                                    <td className='px-3 py-4 sm:w-1/6'>{row?.applied_job?.company || '-'}</td>
-                                    <td className='px-3 py-4 capitalize italic sm:w-1/5'>
-                                        {row?.applied_job?.applied_by?.name || '-'}
-                                    </td>
-                                    <td className='px-3 py-4 capitalize font-semibold sm:w-1/6'>
-                                        {row?.applied_job?.vertical_name || '-'}
-                                    </td>
-                                    <td className='px-3 py-4 capitalize font-semibold italic sm:w-1/6'>
-                                        {row?.candidate?.name || '-'}
-                                    </td>
-                                    <td className='px-3 py-4'>
-                                        <Badge label={row?.applied_job?.tech_stack || '-'} />
-                                    </td>
-                                    <td className='px-3 py-4'>
-                                        <Badge
-                                            label={row?.status?.name || '-'}
-                                            classes='!font-semibold !bg-green-100 !text-green-700 border border-green-500'
-                                        />
-                                    </td>
-                                    <td className='px-3 py-4'>{row?.phase?.name || '-'}</td>
-                                    <td className='px-3 py-4'>{formatDate2(row?.created_at)}</td>
-                                    <td className='px-3 py-4'>{formatDate2(row?.updated_at)}</td>
-                                    <td className='px-3 py-4 text-right'>
-                                        <LeadActions lead={row} dispatch={dispatch} />
-                                    </td>
-                                </tr>
-                            ))
-                        ) : (
-                            <EmptyTable cols={8} msg='No leads found yet' />
-                        )}
-                    </tbody>
-                </table>
+                <div className='overflow-x-auto'>
+                    <table className='table-auto w-full overflow-y-auto max-h-[500px] text-sm text-left text-[#048C8C]'>
+                        <thead className='text-xs uppercase border border-[#048C8C]'>
+                            <tr>
+                                {LEAD_HEADS.map(heading => (
+                                    <th scope='col' className='px-3 py-4' key={heading}>
+                                        {heading}
+                                    </th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data?.leads?.length > 0 && !error ? (
+                                data?.leads?.map((row, idx) => (
+                                    <tr
+                                        className='bg-white border-b border-[#006366] border-opacity-30 hover:bg-slate-100'
+                                        key={row.id}
+                                    >
+                                        <td className='px-3 py-4 w-10'>{idx + 1}</td>
+                                        <td className='px-3 py-4 sm:w-1/5'>{row?.applied_job?.title || '-'}</td>
+                                        <td className='px-3 py-4 sm:w-1/6'>{row?.applied_job?.company || '-'}</td>
+                                        <td className='px-3 py-4 capitalize italic sm:w-1/5'>
+                                            {row?.applied_job?.applied_by?.name || '-'}
+                                        </td>
+                                        <td className='px-3 py-4 capitalize font-semibold sm:w-1/6'>
+                                            {row?.applied_job?.vertical_name || '-'}
+                                        </td>
+                                        <td className='px-3 py-4 capitalize font-semibold italic sm:w-1/6'>
+                                            {row?.candidate?.name || '-'}
+                                        </td>
+                                        <td className='px-3 py-4'>
+                                            <Badge label={row?.applied_job?.tech_stack || '-'} />
+                                        </td>
+                                        <td className='px-3 py-4'>
+                                            <Badge
+                                                label={row?.status?.name || '-'}
+                                                classes='!font-semibold !bg-green-100 !text-green-700 border border-green-500'
+                                            />
+                                        </td>
+                                        <td className='px-3 py-4'>{row?.phase?.name || '-'}</td>
+                                        <td className='px-3 py-4'>{formatDate2(row?.created_at)}</td>
+                                        <td className='px-3 py-4'>{formatDate2(row?.updated_at)}</td>
+                                        <td className='px-3 py-4 text-right'>
+                                            <LeadActions lead={row} dispatch={dispatch} />
+                                        </td>
+                                    </tr>
+                                ))
+                            ) : (
+                                <EmptyTable cols={8} msg='No leads found yet' />
+                            )}
+                        </tbody>
+                    </table>
+                </div>
                 {data?.pages > 1 && (
                     <Paginated
                         pages={data?.pages ?? Math.ceil(data.total / 25)}
