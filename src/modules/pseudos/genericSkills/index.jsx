5b0d069ccd4b2f6ceedb4614fabec2cd9b4166b4
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-09-26 16:55:16 +0500

    feat: make `genericSkills` module responsive

--- src/modules/pseudos/genericSkills/index.jsx
+++ src/modules/pseudos/genericSkills/index.jsx
@@ -29,10 +29,12 @@ const GenericSkills = () => {
 
     if (isLoading) return <Loading />
     return (
-        <div className='max-w-full overflow-x-auto mb-14 px-5'>
-            <div className='flex items-center py-6 justify-between'>
-                <div className='flex space-x-4 items-center'>
-                    <Searchbox query={query} setQuery={setQuery} />
+        <div className='mb-14 md:px-5 px-3'>
+            <div className='flex items-center md:py-6 py-3 justify-between'>
+                <div className='flex flex-col flex-wrap md:flex-row gap-2 md:items-center'>
+                    <div className='flex gap-2'>
+                        <Searchbox query={query} setQuery={setQuery} />
+                    </div>
                     {can('create_generic_skill') && (
                         <Button label='Create Generic Skill' fit icon={CreateIcon} onClick={() => handleClick(null)} />
                     )}
@@ -41,8 +43,8 @@ const GenericSkills = () => {
             <div className='grid grid-cols-2 gap-2 md:grid-cols-4'>
                 {data?.skills?.length > 0 && !error ? (
                     data?.skills?.map((row, idx) => (
-                        <div className='bg-white border border-[#048C8C] rounded-md p-4 relative' key={idx}>
-                            <h2 className='text-lg'>{row?.name ?? 'Not Specified'}</h2>
+                        <div className='bg-white border border-[#048C8C] rounded-md md:p-4 p-2 relative' key={idx}>
+                            <h2 className='md:text-lg'>{row?.name ?? 'Not Specified'}</h2>
                             {can('edit_generic_skill') && can('delete_generic_skill') && (
                                 <GenericSkillActions id={row?.id} mutate={mutate} edit={() => handleClick(row)} />
                             )}
