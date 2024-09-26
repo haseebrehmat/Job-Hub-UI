e51af414ae37c043a002eb8250ee4ed3f88491d3
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-09-26 18:56:26 +0500

    feat: make candidate `Skills` & `Tools` components responsive

--- src/modules/leadManagement/components/Skills/index.jsx
+++ src/modules/leadManagement/components/Skills/index.jsx
@@ -6,21 +6,21 @@ import { getWidthClass } from '@utils/helpers'
 
 const Skills = ({ data }) => (
     <div className='text-[#006366] '>
-        <div className='flex flex-row justify-center'>
-            <div className='border border-1 p-3 m-2 text-center h-fit bg-[#EDFDFB] text-[#1E6570] flex justify-center rounded-xl shadow-lg hover:bg-[#e0fcf8] hover:transform hover:scale-[110%] z-10'>
+        <div className='flex flex-col md:flex-row md:justify-center'>
+            <div className='border border-1 p-2 md:p-3 md:m-2 text-center h-fit bg-[#EDFDFB] text-[#1E6570] flex justify-center rounded-xl shadow-lg hover:bg-[#e0fcf8] hover:transform hover:scale-[110%] z-10'>
                 <div className='flex flex-col justify-center items-center'>
                     <p className='text-md font-semibold uppercase'>Skills</p>
                 </div>
             </div>
-            <div className='flex flex-col w-full px-12 bg-slate-100 py-8 rounded-3xl -ml-8'>
+            <div className='flex flex-col w-full px-8 md:px-12 bg-slate-100 py-4 md:py-8 rounded-3xl md:-ml-8'>
                 {data?.candidates?.skills.map((item, index) => (
                     <div key={index}>
-                        <div className='mb-8 shadow-xl hover:translate hover:scale-[101%]'>
+                        <div className='md:mb-8 mb-3 shadow-xl hover:translate hover:scale-[101%]'>
                             <div className='flex flex-row justify-between mb-1'>
                                 <p className='text-sm uppercase'>{item.name}</p>
                                 <p className='text-sm uppercase'>{item.level}/5</p>
                             </div>
-                            <hr className='h-1  bg-gray-300 z-10' />
+                            <hr className='h-1 bg-gray-300 z-10' />
                             <hr className={`h-1 bg-teal-700 -mt-1 ${getWidthClass(item.level)}`} />
                         </div>
                     </div>
