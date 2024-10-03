d4f6ae2c6114665659fac789013aa1640f79943e
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-10-03 20:09:57 +0500

    fix: sidebar issues in mobile

--- src/modules/layout/components/SidebarItems/index.jsx
+++ src/modules/layout/components/SidebarItems/index.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 import { menuItems } from '@constants/layout'
 import { OpenSubMenuIcon } from '@icons'
 
-const SidebarItems = () => {
+const SidebarItems = ({ close }) => {
     const [subMenu, setSubMenu] = useState({ jobs: false, management: false })
     const handleSubMenu = key => setSubMenu({ ...subMenu, [key]: !subMenu[key] })
 
@@ -14,9 +14,16 @@ const SidebarItems = () => {
             <div key={item.label}>
                 <div
                     className='flex items-center justify-between text-[#003C40] rounded border-2 hover:text-[#003C40] border-solid border-[#EDFFFB] hover:border-2 hover:border-solid hover:border-[#048C8C] active:border-2 active:border-solid active:border-[#048C8C] cursor-pointer'
-                    onClick={() => handleSubMenu(item.key)}
+                    onClick={() => {
+                        handleSubMenu(item.key)
+                        if (!item?.subItems) close()
+                    }}
                 >
-                    <Link to={item.link} className='flex items-center text-md p-4'>
+                    <Link
+                        to={item.link}
+                        className='flex items-center text-md p-4'
+                        onClick={item?.subItems ? null : close}
+                    >
                         {item.svg}
                         <span className='ml-3'>{item.label}</span>
                     </Link>
@@ -29,6 +36,7 @@ const SidebarItems = () => {
                             key={subItem.label}
                             to={subItem.link}
                             className='flex items-center text-sm p-4 ml-2 text-[#003C40] rounded border-2 hover:text-[#003C40] border-solid border-[#EDFFFB] hover:border-2 hover:border-solid hover:border-[#048C8C] active:border-2 active:border-solid active:border-[#048C8C]'
+                            onClick={close}
                         >
                             {subItem.svg}
                             <span className='ml-3'>{subItem.label}</span>
