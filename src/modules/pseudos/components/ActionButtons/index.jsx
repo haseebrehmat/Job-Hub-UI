4a02b10e9948fdf91131fc53873916036ecc1fb3
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-10-16 14:43:40 +0500

    fix: handle mutate null val

--- src/modules/pseudos/components/ActionButtons/index.jsx
+++ src/modules/pseudos/components/ActionButtons/index.jsx
@@ -4,13 +4,13 @@ import { Button } from '@components'
 
 import { useNavigate } from 'react-router-dom'
 
-const ActionButtons = ({ form = false, mutate, classes = null }) => {
+const ActionButtons = ({ form = false, mutate = null, classes = null }) => {
     const navigate = useNavigate()
 
     return (
         <div className={`flex items-center md:justify-end gap-2 ${classes}`}>
             {form && <Button label='Update' type='submit' fit fill classes='px-4 md:!px-8' />}
-            <Button label='Refetch' fill fit onClick={() => mutate()} classes='px-4 md:!px-8' />
+            {mutate && <Button label='Refetch' fill fit onClick={() => mutate()} classes='px-4 md:!px-8' />}
             <Button label='Back to pseudos' fit onClick={() => navigate('/pseudos')} classes='px-4 md:!px-6' />
         </div>
     )
