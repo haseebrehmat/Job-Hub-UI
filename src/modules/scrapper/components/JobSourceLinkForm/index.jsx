13a4374134b8f818180716a0a208381a1c433f63
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-10-24 14:54:26 +0500

    feat: close `JobSourceUrlForm` after saving data

--- src/modules/scrapper/components/JobSourceLinkForm/index.jsx
+++ src/modules/scrapper/components/JobSourceLinkForm/index.jsx
@@ -1,10 +1,10 @@
 import { memo, useState } from 'react'
-
+import { Tooltip } from 'react-tooltip'
 import useSWR from 'swr'
 
 import { useMutate } from '@/hooks'
 
-import { Button, CustomSelector, Drawer, Tooltip, Textarea } from '@components'
+import { Button, CustomSelector, Drawer, Textarea } from '@components'
 
 import { saveJobSourceLink } from '@modules/scrapper/api'
 import { fetchJobSources } from '@modules/settings/api'
@@ -43,14 +43,19 @@ const JobSourceLinkForm = ({ show, setShow, mutate, link }) => {
                 resetForm()
                 setFields([])
             }
+            setShow(false)
         }
     )
     return (
-        <Drawer show={show} setShow={setShow} w='400px'>
+        <Drawer
+            show={show}
+            setShow={setShow}
+            w='600px'
+            dir='bottom'
+            header={`${link?.id ? 'Edit' : 'Create'} Job Source Link / URL`}
+        >
             <form onSubmit={handleSubmit}>
                 <div className='grid grid-flow-row gap-2'>
-                    <p className='font-medium text-xl'>{link?.id ? 'Edit' : 'Create'} Job Source Link / URL</p>
-                    <hr className='mb-2' />
                     {isLoading ? (
                         <div className='text-sm text-gray-600'>Loading....</div>
                     ) : error ? (
@@ -69,33 +74,25 @@ const JobSourceLinkForm = ({ show, setShow, mutate, link }) => {
                             )}
                         </>
                     )}
-                    <div className='flex items-center justify-between my-2'>
+                    <div className='flex items-center justify-between'>
                         <p>Links</p>
                         {fields.length < 30 && (
-                            <Tooltip text='Add Link'>
-                                <Button onClick={addField} icon='+' classes='!px-1 !py-0.5' />
-                            </Tooltip>
+                            <>
+                                <Button onClick={addField} icon='+' classes='!px-0.5 !py-0 add-field !w-fit' />
+                                <Tooltip anchorSelect='.add-field' content='Add Link' />
+                            </>
                         )}
                     </div>
                     {fields.map((field, index) => (
-                        <div key={index} className='flex flex-col space-y-2'>
-                            <div className='flex items-center my-1'>
-                                <Textarea
-                                    rows={2}
-                                    ph={`Enter URL ${index + 1}`}
-                                    value={field.link}
-                                    required
-                                    classes='mr-1'
-                                    onChange={e => handleFieldChange(index, e.target.value)}
-                                />
-                                <Tooltip text='Re-move Link'>
-                                    <Button
-                                        classes='border-0 !text-lg !w-6 !h-6'
-                                        icon={ValidateFalseIcon}
-                                        onClick={() => removeField(index)}
-                                    />
-                                </Tooltip>
-                            </div>
+                        <div key={index} className='flex items-center gap-1 text-sm py-1'>
+                            <Textarea
+                                rows={1}
+                                ph={`Enter URL ${index + 1}`}
+                                value={field.link}
+                                required
+                                classes='mr-1'
+                                onChange={e => handleFieldChange(index, e.target.value)}
+                            />
                             <CustomSelector
                                 options={JOB_TYPES_OPTIONS}
                                 selectorValue={parseJobType(field.job_type)}
@@ -103,11 +100,19 @@ const JobSourceLinkForm = ({ show, setShow, mutate, link }) => {
                                 placeholder='Select job type'
                                 required
                             />
+                            <Button
+                                classes={`border-0 !text-lg !w-6 !h-6 remove-${index}-field`}
+                                icon={ValidateFalseIcon}
+                                onClick={() => removeField(index)}
+                            />
+                            <Tooltip anchorSelect={`.remove-${index}-field`} content={`Remove Link - ${index + 1}`} />
                         </div>
                     ))}
-                    <div className='pt-4 space-y-2'>
-                        {submitButtonShow && <Button label={link?.id ? 'Update' : 'Submit'} type='submit' fill />}
-                        <Button label='Cancel' onClick={() => setShow(false)} />
+                    <div className='pt-1 flex items-center justify-end gap-2'>
+                        {submitButtonShow && (
+                            <Button label={link?.id ? 'Update' : 'Submit'} fit type='submit' fill classes='!px-6' />
+                        )}
+                        <Button label='Cancel' onClick={() => setShow(false)} fit classes='!px-6' />
                     </div>
                 </div>
             </form>
