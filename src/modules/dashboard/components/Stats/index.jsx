2a3d1dff0e19066134ca4138346ff0785bd87a15
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-11-08 16:45:02 +0500

    feat: handle loading & error status in trending_job_stats api in dashboard

--- src/modules/dashboard/components/Stats/index.jsx
+++ src/modules/dashboard/components/Stats/index.jsx
@@ -16,23 +16,27 @@ import { isSuper } from '@utils/helpers'
 const Dashboard = () => {
     const allowed = isSuper()
 
-    const { data } = useSWR(`/api/job_portal/trending_jobs_stats/`, fetchJobStats)
+    const { data, isLoading, error } = useSWR(`/api/job_portal/trending_jobs_stats/`, fetchJobStats)
 
-    return allowed ? (
-        <div className='grid lg:grid-cols-2'>
-            <JobsComparison data={data?.jobs?.month} />
-            <ThrivingSources data={data?.thriving_sources?.month} relative={data?.thriving_source_status} />
-            <DecliningSources data={data?.declining_sources?.month} relative={data?.declining_source_status} />
-            <ThrivingTechnologies
-                data={data?.thriving_tech_stacks?.month}
-                relative={data?.thriving_tech_stack_status}
-            />
-            <DecliningTechnologies
-                data={data?.declining_tech_stacks?.month}
-                relative={data?.declining_tech_stack_status}
-            />
-            <ThrivingJobTitles data={data?.thriving_titles?.month} />
-        </div>
+    return allowed && !error ? (
+        isLoading ? (
+            <h1 className='mx-auto p-4 text-[#4f9d9b] text-lg'>Loading Trending Jobs Stats....</h1>
+        ) : (
+            <div className='grid lg:grid-cols-2'>
+                <JobsComparison data={data?.jobs?.month} />
+                <ThrivingSources data={data?.thriving_sources?.month} relative={data?.thriving_source_status} />
+                <DecliningSources data={data?.declining_sources?.month} relative={data?.declining_source_status} />
+                <ThrivingTechnologies
+                    data={data?.thriving_tech_stacks?.month}
+                    relative={data?.thriving_tech_stack_status}
+                />
+                <DecliningTechnologies
+                    data={data?.declining_tech_stacks?.month}
+                    relative={data?.declining_tech_stack_status}
+                />
+                <ThrivingJobTitles data={data?.thriving_titles?.month} />
+            </div>
+        )
     ) : null
 }
 
