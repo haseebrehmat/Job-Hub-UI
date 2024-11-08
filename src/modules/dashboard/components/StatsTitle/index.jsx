626442edd3487280d2903bd6e7ac8899590bd259
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-11-08 19:08:57 +0500

    update: title UI

--- src/modules/dashboard/components/StatsTitle/index.jsx
+++ src/modules/dashboard/components/StatsTitle/index.jsx
@@ -2,8 +2,8 @@ import { memo } from 'react'
 
 const StatsTitle = ({ title = 'Statistics', sub = 'Listing of Stats' }) => (
     <>
-        <p className='text-lg text-gray-700 border-b font-semibold'>{title}</p>
-        <small className='text-gray-500'>{sub}</small>
+        <p className='text-2xl text-gray-700 border-b'>{title}</p>
+        <small className='text-gray-600 -skew-x-[5deg] my-1'>{sub}</small>
     </>
 )
 
