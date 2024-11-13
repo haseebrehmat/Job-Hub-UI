07d3cf1d143c238134d29baa59664f238117cc9f
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-11-13 21:30:01 +0500

    update: add quarters in `useAnalyticsStore` store

--- src/stores/analytics.js
+++ src/stores/analytics.js
@@ -1,14 +1,16 @@
 import { create } from 'zustand'
 
-import { MONTHS } from '@constants/analytics'
+import { MONTHS, QUARTERS } from '@constants/analytics'
 
 export const useAnalyticsStore = create(set => ({
     // ------------------- Getters ------------------- //
     techStack: {
         months: MONTHS.reduce((acc, month) => ({ ...acc, [month.abr]: true }), {}),
+        quarters: QUARTERS.reduce((acc, month) => ({ ...acc, [month.key]: true }), {}),
     },
     category: {
         months: MONTHS.reduce((acc, month) => ({ ...acc, [month.abr]: true }), {}),
+        quarters: QUARTERS.reduce((acc, month) => ({ ...acc, [month.key]: true }), {}),
     },
 
     // ------------------- Setters ------------------- //
@@ -21,6 +23,14 @@ export const useAnalyticsStore = create(set => ({
                     months: { ...state.techStack.months, [key]: !state.techStack.months[key] },
                 },
             })),
+        quarters: key =>
+            set(state => ({
+                ...state,
+                techStack: {
+                    ...state.techStack,
+                    quarters: { ...state.techStack.quarters, [key]: !state.techStack.quarters[key] },
+                },
+            })),
     },
     toggleCategory: {
         months: key =>
@@ -29,6 +39,14 @@ export const useAnalyticsStore = create(set => ({
                 category: {
                     ...state.category,
                     months: { ...state.category.months, [key]: !state.category.months[key] },
+                },
+            })),
+        quarters: key =>
+            set(state => ({
+                ...state,
+                category: {
+                    ...state.category,
+                    quarters: { ...state.category.quarters, [key]: !state.category.quarters[key] },
                 },
             })),
     },
