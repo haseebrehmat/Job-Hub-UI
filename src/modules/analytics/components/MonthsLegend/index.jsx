854a1e84e8d7b1cd9fdc5cc99d9c3079d4a9faf3
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-11-13 16:46:44 +0500

    feat: add tooltip in toggle months

--- src/modules/analytics/components/MonthsLegend/index.jsx
+++ src/modules/analytics/components/MonthsLegend/index.jsx
@@ -1,4 +1,5 @@
 import { memo } from 'react'
+import { Tooltip } from 'react-tooltip'
 
 import { isset } from '@utils/helpers'
 import { MONTHS } from '@constants/analytics'
@@ -19,10 +20,21 @@ const MonthsLegend = ({ months = null, toggle = null }) => {
                     key={month.key}
                 >
                     {isset(months) && isset(months?.[month.abr]) ? (
-                        <div className='flex items-center cursor-pointer' onClick={() => toggleMonth(month.abr)}>
-                            <span className='px-2.5 py-[8.5px]' style={{ backgroundColor: month.color }} />
-                            {months?.[month.abr] ? Checkedbox : unCheckedbox}
-                        </div>
+                        <>
+                            <div
+                                className='flex items-center cursor-pointer'
+                                onClick={() => toggleMonth(month.abr)}
+                                id={month.key}
+                            >
+                                <span className='px-2.5 py-[8.5px]' style={{ backgroundColor: month.color }} />
+                                {months?.[month.abr] ? Checkedbox : unCheckedbox}
+                            </div>
+                            <Tooltip
+                                content={`${months?.[month.abr] ? 'Hide' : 'Show'} - ${month.name} Data`}
+                                anchorSelect={`#${month.key}`}
+                                className='!px-2 !py-1 tracking-wider text-sm'
+                            />
+                        </>
                     ) : (
                         <span className='px-4 py-1.5' style={{ backgroundColor: month.color }} />
                     )}
